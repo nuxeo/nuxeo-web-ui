a09ad995051df34c0e380e6f8b6eaf66da123516
Andre Justo <ajusto@nuxeo.com>
2016-02-24 15:25:45 +0000

    NXP-18830: update tests to follow changes on the new JS client

--- app/test/test-helpers.js
+++ app/test/test-helpers.js
@@ -57,3 +57,25 @@ function waitChanged(el, prop, times) {
 function isHidden(el) {
   return el.style.display === 'none';
 }
+
+function login(server, loginFixture) {
+  server.respondWith(
+    'POST',
+    '/dummy/api/v1/automation/login',
+    [
+      200,
+      {'Content-Type': 'application/json'},
+      '{"entity-type":"login","username":"Administrator"}'
+    ]
+  );
+  server.respondWith(
+    'GET',
+    '/dummy/api/v1/repo/default/user/Administrator?',
+    [
+      200,
+      {'Content-Type': 'application/json'},
+      '{"entity-type":"user","username":"Administrator"}'
+    ]
+  );
+  return loginFixture.connect();
+}
