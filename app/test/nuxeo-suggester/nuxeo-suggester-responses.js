aaa20e7ee5858376ffbd51bafeff12ebf18ba457
Gabriel Barata <gbarata@nuxeo.com>
2016-03-23 16:28:18 +0000

    NXP-19197: implement new suggester mimicking current suggest search box

---
+++ app/test/nuxeo-suggester/nuxeo-suggester-responses.js
@@ -0,0 +1,22 @@
+var suggesterResponse = [{
+    "id": "5e839dd0-6603-4286-bfdd-ddbdc9aa1918",
+    "label": "Administrator",
+    "type": "document",
+    "icon": "/icons/workspace.gif",
+    "url": "nxdoc/default/5e839dd0-6603-4286-bfdd-ddbdc9aa1918/view_documents"
+  },
+  {
+    "id": "Administrator",
+    "label": "Administrator",
+    "type": "user",
+    "icon": "/icons/user.png",
+    "url": "user/Administrator"
+  }, {
+    "id": "administrators",
+    "label": "administrators",
+    "type": "group",
+    "icon": "/icons/group.png",
+    "url": "group/administrators"
+  }];
+
+var emptyResponse = [];
