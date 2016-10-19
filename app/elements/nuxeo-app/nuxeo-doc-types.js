72f1cfeaaaea8a648ea56b5910003149db93fe3d
Gabriel Barata <gbarata@nuxeo.com>
2016-10-19 10:05:09 +0100

    NXP-20676: move docType registry to nuxeo-app

---
+++ app/elements/nuxeo-app/nuxeo-doc-types.js
@@ -0,0 +1,10 @@
+// temporary extensible doc type registry
+window.nuxeo = window.nuxeo || {};
+window.nuxeo.docTypes = window.nuxeo.docTypes || [
+  {id: 'File', icon: 'icons:description'},
+  {id: 'Note', icon: 'icons:subject'},
+  {id: 'Picture', icon: 'image:image'},
+  {id: 'Video', icon: 'av:video-library'},
+  {id: 'Folder', icon: 'icons:folder'},
+  {id: 'Workspace', icon: 'icons:folder-open'}
+];
