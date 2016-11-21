d1612e54c666eab6c0889ec206efa70c4721d290
Jenkins Nuxeo <jenkins@nuxeo.com>
2016-11-21 11:19:07 +0000

    Update 2016-11-21T11:18:57.923Z

--- elements/nuxeo-app/nuxeo-doc-types.js
+++ elements/nuxeo-app/nuxeo-doc-types.js
@@ -1,7 +1,7 @@
 // temporary extensible doc type registry
 window.nuxeo = window.nuxeo || {};
 window.nuxeo.importBlacklist = window.nuxeo.importBlacklist || [
-  'Workspace', 'Folder', 'OrderedFolder'
+  'Workspace', 'Folder', 'OrderedFolder', 'Collection'
 ];
 window.nuxeo.docTypes = window.nuxeo.docTypes || [
   {id: 'File', icon: '/images/doctypes/document_file.svg'},
