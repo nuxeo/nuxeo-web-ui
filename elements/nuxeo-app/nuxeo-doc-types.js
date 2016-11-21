2c7a3cf9af551999ec5fe38efa3cdc0d660f92ea
Gabriel Barata <gbarata@nuxeo.com>
2016-11-21 20:09:59 +0000

    NXP-20676: make document creation shortcuts use dynamic subtypes

--- elements/nuxeo-app/nuxeo-doc-types.js
+++ elements/nuxeo-app/nuxeo-doc-types.js
@@ -3,12 +3,3 @@ window.nuxeo = window.nuxeo || {};
 window.nuxeo.importBlacklist = window.nuxeo.importBlacklist || [
   'Workspace', 'Folder', 'OrderedFolder', 'Collection'
 ];
-window.nuxeo.docTypes = window.nuxeo.docTypes || [
-  {id: 'File', icon: '/images/doctypes/document_file.svg'},
-  {id: 'Note', icon: '/images/doctypes/document_note.svg'},
-  {id: 'Picture', icon: '/images/doctypes/document_picture.svg'},
-  {id: 'Video', icon: '/images/doctypes/document_video.svg'},
-  {id: 'Audio', icon: '/images/doctypes/document_audio.svg'},
-  {id: 'Folder', icon: '/images/doctypes/container.svg'},
-  {id: 'Workspace', icon: '/images/doctypes/container.svg'}
-];
