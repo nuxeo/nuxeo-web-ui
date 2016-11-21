04284133d70e9d91a5039ca441b78a0934948c79
Guillaume Renard <grenard@nuxeo.com>
2016-11-21 13:15:01 +0100

    NXP-20957: adpat FT

--- nuxeo-web-ui-ftest/webdriver/test/features/step_definitions/search.js
+++ nuxeo-web-ui-ftest/webdriver/test/features/step_definitions/search.js
@@ -3,4 +3,5 @@
 module.exports = function () {
   this.Then('I can see the Search panel', () => this.ui.drawer.search.isVisible().should.be.true);
   this.Then('I can see the Search results', () => this.ui.search.isVisible().should.be.true);
+  this.Then('I cannot see the Search results', () => this.ui.search.isVisible().should.be.false);
 };
