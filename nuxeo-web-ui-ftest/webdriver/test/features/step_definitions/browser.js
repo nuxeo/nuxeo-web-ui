f9220be10ebf32d85efe38d4c5f6db4163e9da70
Nelson Silva <nsilva@nuxeo.com>
2016-12-13 10:39:13 +0000

    NXP-21411: npm run test:dev

--- nuxeo-web-ui-ftest/webdriver/test/features/step_definitions/browser.js
+++ nuxeo-web-ui-ftest/webdriver/test/features/step_definitions/browser.js
@@ -1,11 +1,19 @@
 'use strict';
 
+import DocumentTree from '../../pages/ui/tree';
+
 module.exports = function () {
-  this.Then('I can see the tree', () => this.ui.drawer.browser.isVisible().should.be.true);
+  this.Then('I can see the $tab tree', (tab) => {
+    this.ui.drawer._section(tab).isVisible().should.be.true;
+  });
+
+  this.Then('I can see the "$title" $tab tree node', (title, tab) => {
+    this.ui.drawer._section(tab).element('///nuxeo-tree-node//a[text()="' + title + '"]').isVisible().should.be.true;
+  });
 
   this.When('I click "$title" in the $tab tree', (title, tab) => {
     this.ui.drawer._section(tab).click('///nuxeo-tree-node//a[text()="' + title + '"]');
-    driver.waitUntil(() => this.ui.browser.title === title, 5000);
+    this.ui.browser.title.should.equal(title);
   });
 
   this.Then('I can see the "$title" document content', (title) => {
