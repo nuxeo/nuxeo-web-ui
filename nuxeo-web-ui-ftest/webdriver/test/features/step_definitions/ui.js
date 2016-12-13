9ddc36bbee25716742345631c2348a6ab6b5a7a5
Nelson Silva <nsilva@nuxeo.com>
2016-12-13 12:23:57 +0000

    NXP-21230: open drawer before checking username

--- nuxeo-web-ui-ftest/webdriver/test/features/step_definitions/ui.js
+++ nuxeo-web-ui-ftest/webdriver/test/features/step_definitions/ui.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = function () {
-  this.When('I click the "$button" button', (button) => this.ui.drawer.select(button));
-  this.When('I click the "$name" search button', (name) => this.ui.drawer.selectSearch(name));
+  this.When('I click the "$button" button', (button) => this.ui.drawer.open(button));
+  this.When('I click the "$name" search button', (name) => this.ui.drawer.openSearch(name));
   this.When('I select "$option" from the View menu', (option) => this.ui.view(option));
 };
