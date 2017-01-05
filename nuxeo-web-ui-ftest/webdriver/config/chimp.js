6fc4240e20bbacf9dd17cb910079c8742906fa9f
Nelson Silva <nsilva@nuxeo.com>
2017-01-05 17:32:32 +0000

    NXP-21417: add chimp options files for dev and ci

---
+++ nuxeo-web-ui-ftest/webdriver/config/chimp.js
@@ -0,0 +1,11 @@
+module.exports = {
+  // - - - - CHIMP - - - -
+  showXolvioMessages: false,
+  // - - - - CUCUMBER - - - -
+  path: 'test/features',
+  chai: true,
+  // - - - - WEBDRIVER-IO  - - - -
+  webdriverio: {
+    baseUrl: 'http://localhost:8080/nuxeo',
+  },
+};
--- nuxeo-web-ui-ftest/webdriver/scripts/test.js
+++ nuxeo-web-ui-ftest/webdriver/scripts/test.js
@@ -9,26 +9,15 @@ const spawn = require('child_process').spawn;
 const chimpBin = path.resolve(path.join(process.cwd(), '/node_modules/.bin/chimp'));
 
 const jUnitReporter = require('cucumber-junit');
-const screenshots = './target/screenshots/';
 const jsonReport = `./target/cucumber-reports/report.json`;
 const junitReport = `./target/surefire-reports/TEST-report.xml`;
-
-const args = [
-  '--chai',
-  '--screenshotsOnError=true',
-  `--screenshotsPath=${screenshots}`,
-  `--jsonOutput=${jsonReport}`,
-  '--path=test/features',
-  '--webdriverio.saveScreenshotsToDisk=true',
-  '--webdriverio.baseUrl=http://localhost:8080/nuxeo',
-];
 
 function writeJUnitReport(file) {
   mkdirp.sync(path.dirname(file));
   fs.writeFileSync(file, jUnitReporter(fs.readFileSync(jsonReport)));
 }
 
-const chimp = spawn(chimpBin, args);
+const chimp = spawn(chimpBin, ['config/chimp-ci.js']);
 
 chimp.stdout.pipe(process.stdout);
 chimp.stderr.pipe(process.stderr);
