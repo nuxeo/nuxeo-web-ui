eaf6e4d9a143edff5d6192288a1532df8de4bd4a
Adilio Araujo <aaraujo@nuxeo.com>
2017-01-20 15:15:42 +0000

    NXP-21322: improve versions helper (and step definition)

--- nuxeo-web-ui-ftest/webdriver/test/pages/ui/browser/document_versions.js
+++ nuxeo-web-ui-ftest/webdriver/test/pages/ui/browser/document_versions.js
@@ -12,24 +12,20 @@ export default class DocumentVersions extends BasePage {
     return this.el.element('nuxeo-document-create-version #dialog');
   }
 
-  checkDialogVersion(type, value) {
-    switch (type) {
-      case 'minor':
-        return this._equalValue('#nextMinor', value);
-      case 'major':
-        return this._equalValue('#nextMajor', value);
-    }
+  dialogNextMajor() {
+    return this.el.element('#nextMajor').getText();
   }
 
-  checkDialogButtons() {
-    let dismiss = this.dialog.element('paper-button[dialog-dismiss]');
-    let confirm = this.dialog.element('paper-button[dialog-confirm]');
-    console.log(dismiss.isVisible() && confirm.isVisible());
-    return dismiss.isVisible() && confirm.isVisible();
+  dialogNextMinor() {
+    return this.el.element('#nextMinor').getText();
   }
 
-  _equalValue(selector, value) {
-    return this.el.element(selector).getText().should.equal(value);
+  dialogDismissButton() {
+    return this.dialog.element('paper-button[dialog-dismiss]');
+  }
+
+  dialogConfirmButton() {
+    return this.dialog.element('paper-button[dialog-confirm]');
   }
 
 }
