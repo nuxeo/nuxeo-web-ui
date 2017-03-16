fcce8f1d97ed9a5bd4121c12d677121e1142318e
Karin Touchie <ktouchie@nuxeo.com>
2017-03-16 14:10:38 +0100

    NXP-21741: Update Browse test to check Sections and Templates

--- nuxeo-web-ui-ftest/webdriver/test/features/browser.feature
+++ nuxeo-web-ui-ftest/webdriver/test/features/browser.feature
@@ -14,3 +14,11 @@ Feature: Browser
     Then I can see the "Workspaces" browser tree node
     When I click "Workspaces" in the browser tree
     Then I can see the "Workspaces" document
+    When I click "Domain" in the browser tree
+    Then I can see the "Sections" browser tree node
+    When I click "Sections" in the browser tree
+    Then I can see the "Sections" document
+    When I click "Domain" in the browser tree
+    Then I can see the "Templates" browser tree node
+    When I click "Templates" in the browser tree
+    Then I can see the "Templates" document
--- nuxeo-web-ui-ftest/webdriver/test/features/step_definitions/browser.js
+++ nuxeo-web-ui-ftest/webdriver/test/features/step_definitions/browser.js
@@ -7,8 +7,8 @@ module.exports = function () {
       this.ui.drawer._section(tab).element(`///nuxeo-tree-node//a[text()="${title}"]`).waitForVisible());
 
   this.When('I click "$title" in the $tab tree', (title, tab) => {
-    this.ui.drawer._section(tab).element(`///nuxeo-tree-node//a[text()="${title}"]`).waitForVisible();
-    this.ui.drawer._section(tab).click(`///nuxeo-tree-node//a[text()="${title}"]`);
+    this.ui.drawer._section(tab).element(`///nuxeo-tree-node//a[text()="${title}"]|//*[@id="navTree"]//span[text()="${title}"]`).waitForVisible();
+    this.ui.drawer._section(tab).click(`///nuxeo-tree-node//a[text()="${title}"]|//*[@id="navTree"]//span[text()="${title}"]`);
   });
 
   this.Then('I can see the "$title" document', (title) => this.ui.browser.hasTitle(title).should.be.true);
