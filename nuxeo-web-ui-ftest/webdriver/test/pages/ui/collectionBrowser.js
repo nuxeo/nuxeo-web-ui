eaa395d72389f84857f0db2bdd3a406d7a680880
Guillaume Renard <grenard@nuxeo.com>
2017-03-31 19:24:15 +0200

    NXP-21806: fix tests

--- nuxeo-web-ui-ftest/webdriver/test/pages/ui/collectionBrowser.js
+++ nuxeo-web-ui-ftest/webdriver/test/pages/ui/collectionBrowser.js
@@ -9,14 +9,14 @@ export default class CollectionBrowser {
 
   waitForHasMember(doc) {
     driver.waitUntil(() => {
-      const rows = this.page.elements('#datatable #list nuxeo-data-table-row');
+      const rows = this.page.elements('nuxeo-data-table #list nuxeo-data-table-row');
       return rows.value.some((row) => row.getText(`nuxeo-data-table-cell a.title`).trim() === doc.title);
     }, 'The document does not appear in the collection');
     return true;
   }
 
   clickMember(doc) {
-    const rows = this.page.elements('#datatable #list nuxeo-data-table-row');
+    const rows = this.page.elements('nuxeo-data-table #list nuxeo-data-table-row');
     return rows.value.some((row) => {
       if (row.getText(`nuxeo-data-table-cell a.title`).trim() === doc.title) {
         row.click();
