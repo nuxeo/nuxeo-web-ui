582399e4c99d0f9872ec95d3dd1106f92a5b083e
Gabriel Barata <gbarata@nuxeo.com>
2017-05-24 17:12:56 +0100

    NXP-22262: upgrade to chimp 0.49.0

--- nuxeo-web-ui-ftest/webdriver/test/features/step_definitions/browser.js
+++ nuxeo-web-ui-ftest/webdriver/test/features/step_definitions/browser.js
@@ -11,5 +11,8 @@ module.exports = function () {
     this.ui.drawer._section(tab).click(`///nuxeo-tree-node//a[text()="${title}"]|//*[@id="navTree"]//span[text()="${title}"]`);
   });
 
-  this.Then('I can see the "$title" document', (title) => this.ui.browser.hasTitle(title).should.be.true);
+  this.Then('I can see the "$title" document', (title) => {
+    this.ui.browser.waitForVisible();
+    this.ui.browser.hasTitle(title).should.be.true;
+  });
 };
