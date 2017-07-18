805f0357da86dd8d2634e0421d7a20d7e80a62bd
Karin Touchie <ktouchie@nuxeo.com>
2017-07-18 17:18:19 +0200

    NXP-22272: Separate User test scenarios

--- nuxeo-web-ui-ftest/webdriver/test/features/step_definitions/user.js
+++ nuxeo-web-ui-ftest/webdriver/test/features/step_definitions/user.js
@@ -17,18 +17,22 @@ module.exports = function () {
 
   this.Then(/^I can search for the user "([^"]*)"$/, (username) => {
     this.ui.user.searchFor(username);
-    this.ui.user.searchResult(username).waitForVisible();
-    this.ui.user.searchResult(username).isVisible().should.be.true;
+    this.ui.user.searchResult(username).waitForVisible().should.be.true;
   });
 
   this.Then(/^I can edit the user "([^"]*)" with the following properties:$/, (username, table) => {
+    this.ui.user.searchFor(username);
+    this.ui.user.searchResult(username).waitForVisible();
     this.ui.user.searchResult(username).click();
     this.ui.user.editUserButton.click();
     this.ui.user.fillMultipleValues(table, this.ui.user.editUserDialog);
     this.ui.user.editUserDialogButton.click();
   });
 
-  this.Then(/^I can delete the user$/, () => {
+  this.Then(/^I can delete the user "([^"]*)"$/, (username) => {
+    this.ui.user.searchFor(username);
+    this.ui.user.searchResult(username).waitForVisible();
+    this.ui.user.searchResult(username).click();
     this.ui.user.deleteUserButton.click();
     this.ui.user.confirmDeleteUserButton.click();
   });
