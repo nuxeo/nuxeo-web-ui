40cb710aa5dee13367418914383d97bc052b7269
Gabriel Barata <gbarata@nuxeo.com>
2017-07-31 16:17:16 +0100

    NXP-22711: fix loading page on Safari

--- src/main/resources/web/nuxeo.war/ui/app.js
+++ src/main/resources/web/nuxeo.war/ui/app.js
@@ -1,6 +1,11 @@
 function finishLazyLoading() {
   window.Polymer = window.Polymer || {dom: 'lazyRegister'};
 
+  var clear = function() {
+    var loadEl = document.getElementById('loader');
+    loadEl.remove();
+  }
+
   var checkImportLoaded = function() {
     var links = document.querySelectorAll('link[rel="import"]');
     for (var i = 0; i < links.length; i++) {
@@ -9,13 +14,20 @@ function finishLazyLoading() {
         return;
       }
     }
-    var loadEl = document.getElementById('loader');
-    loadEl.remove();
+    clear();
   };
 
   window.setTimeout(function() {
     document.getElementById('loader').style.display = "block";
   }, 500);
+
+  // hack for known webkit issue: https://github.com/webcomponents/webcomponentsjs/issues/499
+  var ua = navigator.userAgent.toLowerCase();
+  if (window.HTMLImports && ua.indexOf('safari') > -1 && ua.indexOf('chrome') === -1) {
+    window.HTMLImports.whenReady(function() {
+      setTimeout(clear);
+    });
+  }
 
   checkImportLoaded();
 }
