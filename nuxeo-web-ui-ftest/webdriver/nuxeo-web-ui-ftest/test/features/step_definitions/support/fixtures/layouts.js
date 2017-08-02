963a4f33813d66936e746447999b445d11772107
Gabriel Barata <gbarata@nuxeo.com>
2017-08-02 15:37:05 +0100

    NXP-22816: add widget getter and setter for nuxeo-dropzone

--- nuxeo-web-ui-ftest/webdriver/nuxeo-web-ui-ftest/test/features/step_definitions/support/fixtures/layouts.js
+++ nuxeo-web-ui-ftest/webdriver/nuxeo-web-ui-ftest/test/features/step_definitions/support/fixtures/layouts.js
@@ -1,4 +1,5 @@
 import { FieldRegistry } from '../services/field_registry';
+import path from 'path';
 
 global.fieldRegistry = new FieldRegistry();
 global.fieldRegistry.register('nuxeo-input',
@@ -24,9 +25,8 @@ global.fieldRegistry.register('nuxeo-textarea',
                               (element) => element.element('#textarea').getValue(),
                               (element, value) => { element.element('#textarea').setValue(value); });
 global.fieldRegistry.register('nuxeo-user-suggestion',
-                              (element) => {
-                                element.element(`#select2-drop div.select2-search input.select2-input`).getValue();
-                              },
+                              (element) => element.element(`#select2-drop div.select2-search input.select2-input`)
+                                                  .getValue(),
                               (element, value) => {
                                 element.element('nuxeo-select2 div#s2id_select2').click();
                                 driver.waitForVisible(`#select2-drop div.select2-search input.select2-input`);
@@ -48,7 +48,7 @@ global.fieldRegistry.register('nuxeo-directory-suggestion',
                                               li.select2-search-choice:nth-child(${i}) div`).getText();
                                   return multiple;
                                 } else {
-                                  element.element('#select2').getValue();
+                                  return element.element('#select2').getValue();
                                 }
                               },
                               (element, value) => {
@@ -70,9 +70,7 @@ global.fieldRegistry.register('nuxeo-directory-suggestion',
                                 }
                               });
 global.fieldRegistry.register('nuxeo-dropdown-aggregation',
-                              (element) => {
-                                element.element('nuxeo-select2 input').getValue();
-                              },
+                              (element) => element.element('nuxeo-select2 input').getValue(),
                               (element, value) => {
                                 element.element('nuxeo-select2 input').click();
                                 driver.element('nuxeo-select2 input').setValue(value);
@@ -80,9 +78,7 @@ global.fieldRegistry.register('nuxeo-dropdown-aggregation',
                                 driver.element(`#select2-drop li.select2-result`).click();
                               });
 global.fieldRegistry.register('nuxeo-select2',
-                              (element) => {
-                                element.element('div ul li input').getValue();
-                              },
+                              (element) => element.element('div ul li input').getValue(),
                               (element, value) => {
                                 element.element('div ul li input').click();
                                 driver.element('div ul li input').setValue(value);
@@ -90,9 +86,8 @@ global.fieldRegistry.register('nuxeo-select2',
                                 driver.element(`//div[text()='${value}' and @class='select2-result-label']`).click();
                               });
 global.fieldRegistry.register('nuxeo-tag-suggestion',
-                              (element) => {
-                                element.element('nuxeo-tag-suggestion nuxeo-select2 div ul li input').getValue();
-                              },
+                              (element) => element.element('nuxeo-tag-suggestion nuxeo-select2 div ul li input')
+                                                  .getValue(),
                               (element, value) => {
                                 element.element('nuxeo-tag-suggestion nuxeo-select2 div ul li input').click();
                                 driver.element('nuxeo-tag-suggestion nuxeo-select2 div ul li input').setValue(value);
@@ -103,18 +98,14 @@ global.fieldRegistry.register('paper-input',
                               (element) => element.element('#input').getValue(),
                               (element, value) => { element.element('#input').setValue(value); });
 global.fieldRegistry.register('paper-radio-button',
-                              (element) => {
-                                element.element('#radioContainer').getAttribute('multiple');
-                              },
+                              (element) => element.element('#radioContainer').getAttribute('multiple'),
                               (element, value) => {
                                 if (value) {
                                   element.element('#radioContainer').click();
                                 }
                               });
 global.fieldRegistry.register('paper-checkbox',
-                              (element) => {
-                                element.getAttribute('checked');
-                              },
+                              (element) => element.getAttribute('checked'),
                               (element, value) => {
                                 if ((value === false && element.getAttribute('checked') === 'true') ||
                                   (value === true && element.getAttribute('checked') === 'null')) {
@@ -122,14 +113,19 @@ global.fieldRegistry.register('paper-checkbox',
                                 }
                               });
 global.fieldRegistry.register('nuxeo-checkbox-aggregation',
-                              (element) => {
-                                element.element(
+                              (element) => element.element(
                                   `///div[@id='checkboxLabel']/parent::paper-checkbox`
-                                ).getAttribute('aria-checked');
-                              },
+                                ).getAttribute('aria-checked'),
                               (element, value) => {
                                 driver.waitForVisible(`//div[@id='checkboxLabel' and contains(., '${value}')]`);
                                 element.element(`///div[@id='checkboxLabel' and contains(., '${value}')]`).click();
+                              });
+global.fieldRegistry.register('nuxeo-dropzone',
+                              (element) => element.element(`///input[@id='input']`).getValue(),
+                              (element, value) => {
+                                element.waitForExist(`///input[@id='input']`);
+                                browser.chooseFile(`//nuxeo-dropzone/input[@id='input']`,
+                                                   path.resolve(fixtures.blobs.get(value)));
                               });
 global.fieldRegistry.register('generic',
                               (element) => element.getText(),
