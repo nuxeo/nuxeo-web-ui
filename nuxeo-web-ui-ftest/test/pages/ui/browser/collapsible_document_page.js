975e97dd996141055ddaa0f6f2474395db407307
Pedro Costa <pcosta@nuxeo.com>
2017-09-27 16:43:19 +0100

    NXP-22997: adapts tests to the new collapsible layout

---
+++ nuxeo-web-ui-ftest/test/pages/ui/browser/collapsible_document_page.js
@@ -0,0 +1,42 @@
+'use strict';
+
+import DocumentPage from './document_page';
+import DocumentMetadata from './document_metadata';
+import DocumentView from './document_view';
+import DocumentVersions from './document_versions';
+
+export default class CollapsibleDocumentPage extends DocumentPage {
+
+  constructor(selector, docType) {
+    super(selector);
+    this.docType = docType;
+  }
+
+  get detailsCard() {
+    return this.el.element('#detailsCard');
+  }
+
+  get metadata() {
+    this.expandDetailsCard();
+    return super.metadata;
+  }
+
+  get versions() {
+    this.expandDetailsCard();
+    return super.versions;
+  }
+
+  expandDetailsCard() {
+    this.detailsCard.waitForVisible();
+    if (this.detailsCard.getAttribute('opened') === 'false') {
+      this.detailsCard.click('h3');
+    }
+  }
+
+  collapseDetailsCard() {
+    this.detailsCard.waitForVisible();
+    if (this.detailsCard.getAttribute('opened')) {
+      this.detailsCard.click('h3');
+    }
+  }
+}
