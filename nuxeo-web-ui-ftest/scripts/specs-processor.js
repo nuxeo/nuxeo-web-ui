4b64459e25142c74eacec7d6c1e5c2ef91f84882
Karin Touchie <ktouchie@nuxeo.com>
2017-09-28 14:05:50 +0200

    NXP-23172: Inject only tagged features when using --tags=@watch

--- nuxeo-web-ui-ftest/scripts/specs-processor.js
+++ nuxeo-web-ui-ftest/scripts/specs-processor.js
@@ -1,9 +1,18 @@
-module.exports = (argv) => {
-  const args = require('minimist')(argv.slice(2));
-  if (args['specs']) {
-    return args['specs'].split(',');
-  } else {
-    return ['./test/features/*.feature'];
-  }
-};
+ const Finder = require('fs-finder');
+ const fs = require('fs');
 
+module.exports = (argv) => {
+  let features = [];
+  const args = require('minimist')(argv.slice(2));
+  if (args['cucumberOpts']['tags']) {
+    const files = Finder.from('./test/features').findFiles('*.feature');
+    files.forEach((file) => {
+      if (fs.readFileSync(file, 'utf8').includes(args.cucumberOpts.tags)) {
+        features.push(file);
+      }
+    });
+  } else {
+    features = './test/features/*.feature';
+  }
+  return features;
+};
