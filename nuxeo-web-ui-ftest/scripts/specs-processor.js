5cea2b12932fbb2300ce8162a76f48fa975d3003
Karin Touchie <ktouchie@nuxeo.com>
2017-09-29 12:29:51 +0200

    NXP-23172: Fix tag check

--- nuxeo-web-ui-ftest/scripts/specs-processor.js
+++ nuxeo-web-ui-ftest/scripts/specs-processor.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 module.exports = (argv) => {
   let features = [];
   const args = require('minimist')(argv.slice(2));
-  if (args['cucumberOpts']['tags']) {
+  if (args['cucumberOpts'] && args['cucumberOpts']['tags']) {
     const files = Finder.from('./test/features').findFiles('*.feature');
     features = files.filter(file => fs.readFileSync(file, 'utf8').includes(args.cucumberOpts.tags));
   } else {
