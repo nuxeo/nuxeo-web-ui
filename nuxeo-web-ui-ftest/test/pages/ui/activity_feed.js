8f3263dd1149de4efea449268e3eb1713f893b00
Andre Justo <ajusto@nuxeo.com>
2018-01-08 16:45:52 +0000

    NXP-23505: re-enable Versioning tests

--- nuxeo-web-ui-ftest/test/pages/ui/activity_feed.js
+++ nuxeo-web-ui-ftest/test/pages/ui/activity_feed.js
@@ -5,7 +5,7 @@ import BasePage from '../base';
 export default class ActivityFeed extends BasePage {
 
   getActivity(activity) {
-    return this.el.element(`///*[text()="${activity}"]`);
+    return this.el.elementByTextContent('.value span', activity);
   }
 
 }
