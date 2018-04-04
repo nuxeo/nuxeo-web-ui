cf834a31e7208b2b2e0e09b51910ab716ef860d1
Gabriel Barata <gbarata@nuxeo.com>
2018-04-04 17:09:40 +0100

    NXP-22588: add functional tests for user authorized applications

---
+++ ftest/web-ui/webdriver/test/features/authorized_apps.feature
@@ -0,0 +1,24 @@
+Feature: Authorized Applications
+
+  Users can manage their authorized applications.
+
+  Background:
+    Given user "John" exists in group "members"
+    And I login as "John"
+    And the following OAuth2 clients exist
+      | name             |
+      | My Client        |
+      | My Second Client |
+    And I have tokens for the following OAuth2 clients
+      | name             |
+      | My Client        |
+      | My Second Client |
+
+  Scenario: View authorized applications
+    When I am on user authorized applications page
+    Then I can only see 2 authorized applications
+    
+  Scenario: Revoke access to an authorized application
+    When I am on user authorized applications page
+    Then I can revoke access for "My Client" application
+    And I can only see 1 authorized application
---
+++ nuxeo-web-ui-ftest/test/pages/ui/oauth2/user_authorized_apps.js
@@ -0,0 +1,38 @@
+'use strict';
+
+import BasePage from '../../base';
+
+class AuthorizedApp {
+  constructor(element) {
+    this.el = element;
+  }
+
+  get name() {
+    return this.el.elements(`nuxeo-data-table-cell`).value[0].getText();
+  }
+
+  get authorizationDate() {
+    return this.el.elements(`nuxeo-data-table-cell`).value[1].getText();
+  }
+
+  revokeButton() {
+    return this.el.element(`paper-icon-button[name="revoke"]`);
+  }
+}
+
+export default class UserAuthorizedApps extends BasePage {
+
+  getApps(appName) {
+    this.el.waitForVisible(`nuxeo-data-table nuxeo-data-table-row`);
+    let apps = this.el.elements(`nuxeo-data-table nuxeo-data-table-row`).value
+                      .splice(1) // skip the header
+                      .map(el => new AuthorizedApp(el)) // and map every element to a wrapper we can work with
+                      .filter(app => !!app.name.trim());
+    // because clients are update after tokens, there might be empty rows that must be filtered
+    if (appName) {
+      apps = apps.filter(app => app.name === appName);
+    }
+    return apps;
+  }
+
+}
