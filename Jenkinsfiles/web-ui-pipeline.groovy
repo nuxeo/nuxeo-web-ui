2e9ec7bae438deaa9e44b4e09e2540d7e9343d26
Guillaume Renard <grenard@nuxeo.com>
2018-04-25 16:45:22 +0200

    NXP-24917: add web ui Jenkins pipeline file

---
+++ Jenkinsfiles/web-ui-pipeline.groovy
@@ -0,0 +1,168 @@
+properties([
+    [$class: 'RebuildSettings', autoRebuild: false, rebuildDisabled: false],
+    [$class: 'ParametersDefinitionProperty', parameterDefinitions:
+        [
+            [$class: 'StringParameterDefinition', defaultValue: '', description: 'Branch to test, fall-backs on $PARENT_BRANCH if not found.', name: 'BRANCH'],
+            [$class: 'StringParameterDefinition', defaultValue: 'master', description: 'The branch to fallback on when $BRANCH is not found.', name: 'PARENT_BRANCH'],
+            [$class: 'StringParameterDefinition', defaultValue: 'SLAVE', description: 'Slave label to be used.', name: 'SLAVE'],
+            [$class: 'BooleanParameterDefinition', defaultValue: false,  description: 'Run npm and bower cache clean?', name: 'CLEAN'],
+            [$class: 'BooleanParameterDefinition', defaultValue: true,  description: 'Should PRs be created if build is successful?', name: 'CREATE_PR'],
+            [$class: 'BooleanParameterDefinition', defaultValue: false, description: 'Should the functionnal tests be skipped?', name: 'SKIP_IT_TESTS'],
+            [$class: 'BooleanParameterDefinition', defaultValue: false, description: 'Should the element\'s tests be skipped?', name: 'SKIP_UNIT_TESTS']
+        ]
+    ],
+    pipelineTriggers([])
+])
+
+currentBuild.setDescription("Branch: $BRANCH -> $PARENT_BRANCH")
+
+def runSauceLabTests(repo, sauceCredentialId) {
+    if (params.SKIP_UNIT_TESTS) {
+        echo 'Tests are skipped'
+    } else {
+        withCredentials([usernamePassword(credentialsId: "$sauceCredentialId", passwordVariable: 'SAUCE_ACCESS_KEY', usernameVariable: 'SAUCE_USERNAME')]) {
+            sh "npm install && npm run lint && ./node_modules/.bin/polymer test -l chrome --plugin sauce --job-name ${repo}-pipeline-$BRANCH --build-number $BUILD_NUMBER"
+        }
+    }
+}
+
+def createPullRequest(repo) {
+    withCredentials([usernamePassword(credentialsId: 'eea4e470-2c5e-468f-ab3a-e6c81fde94c0', passwordVariable: 'GITHUB_PASSWD', usernameVariable: 'GITHUB_TOKEN')]) {
+        sh "curl -u \"$GITHUB_TOKEN:$GITHUB_PASSWD\" -H \"Accept: application/vnd.github.symmetra-preview+json\" -d '{\"title\":\"$BRANCH\",\"base\":\"$PARENT_BRANCH\", \"head\":\"$BRANCH\", \"body\": \"This Pull Request looks good!\"}'  https://api.github.com/repos/nuxeo/${repo}/pulls"
+    }
+}
+
+def rebase_and_merge(branch = BRANCH, base = PARENT_BRANCH) {
+    sh "git rebase origin/${base}"
+    sh "git checkout ${base}"
+    sh "git pull --rebase"
+    sh "git merge --no-ff origin/${branch} --log"
+}
+
+def cloneRebaseAndDir(repo, branch = BRANCH, fallback = PARENT_BRANCH) {
+    try {
+        checkout([
+                $class: 'GitSCM',
+                branches: [[name: "refs/heads/$branch"]],
+                extensions: [
+                    [$class: 'RelativeTargetDirectory', relativeTargetDir: repo],
+                    [$class: 'CleanBeforeCheckout']],
+                userRemoteConfigs: [[url: "git@github.com:nuxeo/${repo}.git"]]])
+        if (fallback) {
+            dir (repo) {
+                rebase_and_merge(branch, fallback)
+            }
+        }
+        return true
+    } catch (err) {
+        echo "branch ${branch} not found in ${repo}"
+        if (fallback != null) {
+            echo "checking out ${fallback}"
+            cloneRebaseAndDir(repo, fallback, null)
+        }
+        return false
+    }
+}
+
+def replaceVersion(dep, branch = BRANCH, base = PARENT_BRANCH) {
+    sh "sed -i 's/${dep}#${base}/${dep}#${branch}/g' bower.json"
+}
+
+timestamps {
+    node(SLAVE) {
+        deleteDir()
+        def el, uiel, webui, plugin
+        if (params.CLEAN) {
+            sh 'npm cache clean && bower cache clean'
+        }
+        stage('nuxeo-elements') {
+            timeout(30) {
+                el = cloneRebaseAndDir('nuxeo-elements')
+                if (el) {
+                    echo 'Need to build nuxeo-elements'
+                    dir('nuxeo-elements') {
+                        sh 'bower install'
+                        runSauceLabTests('nuxeo-elements', 'SAUCE_ELEMENTS_ACCESS_KEY')
+                    }
+                } else {
+                    echo 'No need to build nuxeo-elements'
+                }
+            }
+        }
+        stage('nuxeo-ui-elements') {
+            timeout(30) {
+                uiel = cloneRebaseAndDir('nuxeo-ui-elements')
+                if (uiel || el) {
+                    echo 'Need to build nuxeo-ui-elements'
+                    dir('nuxeo-ui-elements') {
+                        sh 'bower install'
+                        if (el) {
+                            replaceVersion('nuxeo-elements')
+                        }
+                        runSauceLabTests('nuxeo-ui-elements', 'SAUCE_UI_ELEMENTS_ACCESS_KEY')
+                    }
+                } else {
+                    echo 'No need to build nuxeo-ui-elements'
+                }
+            }
+        }
+        def mvnHome = tool 'maven-3.3'
+        def javaHome = tool 'java-8-oracle'
+        withEnv(["JAVA_HOME=${javaHome}", "MAVEN=${mvnHome}/bin", "PATH=${env.JAVA_HOME}/bin:${env.MAVEN}:${env.PATH}"]) {
+            stage('nuxeo-web-ui') {
+                timeout(60) {
+                    webui = cloneRebaseAndDir('nuxeo-web-ui')
+                    if (webui || el || uiel) {
+                        echo 'Need to build nuxeo-web-ui'
+                        dir('nuxeo-web-ui') {
+                            if (el) {
+                                replaceVersion('nuxeo-elements')
+                            }
+                            if (uiel) {
+                                replaceVersion('nuxeo-ui-elements')
+                            }
+                            sh 'mvn clean install'
+                            archive 'target/*.jar'
+                        }
+                    } else {
+                        echo 'No need to build nuxeo-web-ui'
+                    }
+                }
+            }
+            stage('plugin-nuxeo-web-ui') {
+                timeout(60) {
+                    plugin = cloneRebaseAndDir('plugin-nuxeo-web-ui')
+                    if (plugin || el || uiel || plugin) {
+                        echo 'Need to plugin-nuxeo-web-ui'
+                        dir('plugin-nuxeo-web-ui') {
+                            if (!params.SKIP_IT_TESTS) {
+                                sh 'mvn clean install -Pftest'
+                            } else {
+                                sh 'mvn clean install'
+                            }
+                            archive '**/reports/*,**/log/*.log, **/target/cucumber-reports/*.json, **/nxserver/config/distribution.properties, **/failsafe-reports/*, **/target/results/*.html, **/target/screenshots/*.png, marketplace/target/nuxeo-web-ui-marketplace-*-SNAPSHOT.zip'
+                        }
+                    } else {
+                        echo 'No need to build plugin-nuxeo-web-ui'
+                    }
+                }
+            }
+            stage('post-build') {
+                if (params.CREATE_PR) {
+                    if (el) {
+                        createPullRequest('nuxeo-elements')
+                    }
+                    if (uiel) {
+                        createPullRequest('nuxeo-ui-elements')
+                    }
+                    if (webui) {
+                        createPullRequest('nuxeo-web-ui')
+                    }
+                    if (plugin) {
+                        createPullRequest('plugin-nuxeo-web-ui')
+                    }
+                }
+            }
+        }
+    }
+}
