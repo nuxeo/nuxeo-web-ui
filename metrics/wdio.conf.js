554da17e0a18a4e15aeecd2031ea935c6a56400a
Nelson Silva <nsilva@nuxeo.com>
2018-07-26 17:16:29 +0100

    NXP-21673: add metrics profile and report

---
+++ metrics/wdio.conf.js
@@ -0,0 +1,28 @@
+exports.capabilities = {
+  chrome: {
+      desiredCapabilities: {
+          browserName: 'chrome',
+          platform: 'Windows 10',
+          version: '',
+      }
+  },
+  firefox: {
+      desiredCapabilities: {
+          browserName: 'firefox',
+          platform: 'Windows 10',
+          version: '',
+      }
+  },
+  safari: {
+      desiredCapabilities: {
+          browserName: 'safari',
+          version: '',
+      }
+  },
+  edge: {
+      desiredCapabilities: {
+          browserName: 'microsoftedge',
+          version: ''
+      }
+  }
+};
