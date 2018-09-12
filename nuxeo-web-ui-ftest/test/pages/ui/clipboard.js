0bdb00dcb6dffc8d29a7cdee08f1016c3af51898
Guillaume Renard <grenard@nuxeo.com>
2018-09-12 14:24:36 +0200

    NXP-25768: fix document clipboard test

--- nuxeo-web-ui-ftest/test/pages/ui/clipboard.js
+++ nuxeo-web-ui-ftest/test/pages/ui/clipboard.js
@@ -5,6 +5,8 @@ import BasePage from '../base';
 export default class Clipboard extends BasePage {
 
   get nbItems() {
+    // we need to force the clipboard storage reload since we may have by-passed it in the fixture
+    browser.execute(() => document.querySelector('* >>> nuxeo-clipboard >>> #storage').reload());
     const items = this.el.elements(`#list .list-item`).value;
     let count = 0;
     items.forEach((item) => {
