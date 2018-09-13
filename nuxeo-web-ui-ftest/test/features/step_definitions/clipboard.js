38b4ca6983bfb9f5e31a943f2c8b91d2fab6be36
Nuno Cunha <ncunha@nuxeo.com>
2018-09-13 10:48:12 +0100

    NXP-25773: Add functional tests to recently viewed documents.

--- nuxeo-web-ui-ftest/test/features/step_definitions/clipboard.js
+++ nuxeo-web-ui-ftest/test/features/step_definitions/clipboard.js
@@ -1,18 +1,21 @@
 'use strict';
 
 module.exports = function () {
-  this.Given(/^I have a clean clipboard$/, () => fixtures.documents.clearClipboard(this.username));
+  this.Given(/^I have a clean clipboard$/, () => {
+    fixtures.documents.clearLocalStorage(this.username, 'nuxeo-clipboard');
+  });
 
   this.Given(/^I have document with path "(.+)" on clipboard$/, (path) => {
-    fixtures.documents.addToClipboard(path, this.username);
+    fixtures.documents.addToLocalStorage(path, this.username, 'nuxeo-clipboard')
+        .then(() => this.ui.drawer.clipboard.reload());
   });
 
   this.Given(/^I have the following documents on clipboard$/, (table) => {
     const promises = [];
     table.rows().map((row) => {
-      promises.push(fixtures.documents.addToClipboard(row[0], this.username));
+      promises.push(fixtures.documents.addToLocalStorage(row[0], this.username, 'nuxeo-clipboard'));
     });
-    return Promise.all(promises);
+    return Promise.all(promises).then(() => this.ui.drawer.clipboard.reload());
   });
 
   this.When('I click remove button for "$title" document', (title) => {
@@ -36,7 +39,7 @@ module.exports = function () {
 
   this.Then('I can see the clipboard has "$title" document', (title) => {
     this.ui.drawer.clipboard.waitForVisible();
-    this.ui.drawer.clipboard.el.hasElementByTextContent('#list .list-item-title', title);
+    this.ui.drawer.clipboard.el.hasElementByTextContent('#list .list-item-title', title).should.be.true;
   });
   this.Then('I can see the clipboard has "$nb" items', (nb) => {
     this.ui.drawer.clipboard.waitForVisible();
--- nuxeo-web-ui-ftest/test/features/step_definitions/recents.js
+++ nuxeo-web-ui-ftest/test/features/step_definitions/recents.js
@@ -1,6 +1,26 @@
 'use strict';
 
 module.exports = function () {
-  this.Then('I can see the list of recently viewed documents',
-      () => this.ui.drawer.recents.waitForVisible().should.be.true);
+  this.Given(/^I have a clean recently viewed documents list/, () => {
+    fixtures.documents.clearLocalStorage(this.username, 'nuxeo-recent-documents');
+  });
+
+  this.Given(/^I have document with path "(.+)" on the recently viewed documents list/, (path) => {
+    fixtures.documents.addToLocalStorage(path, this.username, 'nuxeo-recent-documents')
+        .then(() => this.ui.drawer.recents.reload());
+  });
+
+  this.Then('I can see the list of recently viewed documents', () => {
+    this.ui.drawer.recents.waitForVisible().should.be.true;
+  });
+
+  this.Then('I can see the list of recently viewed documents has "$nb" items', (nb) => {
+    this.ui.drawer.recents.waitForVisible();
+    this.ui.drawer.recents.nbItems.should.be.equals(parseInt(nb));
+  });
+
+  this.Then('I can see the list of recently viewed documents has "$title" document', (title) => {
+    this.ui.drawer.recents.waitForVisible();
+    this.ui.drawer.recents.el.hasElementByTextContent('#recentDocumentsList .list-item-title', title).should.be.true;
+  });
 };
