7eab4591bcc4192c9fcfaf6bdeb5dcf1a1b012b5
Guillaume Renard <grenard@nuxeo.com>
2018-10-21 16:48:51 +0200

    NXP-24427: add ftests for pusblishing a set of document

---
+++ nuxeo-web-ui-ftest/test/pages/ui/browser/publication_dialog.js
@@ -0,0 +1,33 @@
+import BasePage from '../../base';
+import DocumentVersions from './document_versions';
+
+export default class PublicationDialog extends BasePage {
+  publish(target, rendition, version, override) {
+    // set target
+    const targetSelect = this.el.element('#target');
+    fixtures.layouts.setValue(targetSelect, target);
+    // set rendition
+    if (rendition) {
+      const renditionSelect = this.el.element('#rendition');
+      fixtures.layouts.setValue(renditionSelect, rendition);
+    }
+    // set version
+    if (version) {
+      const versionsList = new DocumentVersions(`${this._selector} #version`);
+      versionsList.toggle.waitForVisible();
+      versionsList.toggle.click();
+      versionsList.listItems.waitForVisible();
+      versionsList.selectVersion(version);
+    }
+    if (override) {
+      const overrideCheckbox = this.el.element('#override');
+      fixtures.layouts.setValue(overrideCheckbox, true);
+    }
+    this.el.waitForEnabled('#publish');
+    this.el.click('#publish');
+  }
+
+  waitForVisible() {
+    return this.el.waitForVisible();
+  }
+}
