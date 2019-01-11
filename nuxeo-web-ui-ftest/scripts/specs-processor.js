dd572766c398cf0bba870d34137cae8233eade01
Nuno Cunha <ncunha@nuxeo.com>
2019-01-11 17:39:22 +0000

    NXP-26633: update tags syntax to tagExpression

--- nuxeo-web-ui-ftest/scripts/specs-processor.js
+++ nuxeo-web-ui-ftest/scripts/specs-processor.js
@@ -4,9 +4,14 @@ const fs = require('fs');
 module.exports = (argv) => {
   let features = [];
   const args = require('minimist')(argv.slice(2));
-  if (args.cucumberOpts && args.cucumberOpts.tags) {
+  /*
+   * XXX
+   * tagExpression is being used only for syntax purposes, since we are not evaluating expressions.
+   * It would only be full featured after this NXP-26660 being addressed.
+   */
+  if (args.cucumberOpts && args.cucumberOpts.tagExpression) {
     const files = Finder.from('./test/features').findFiles('*.feature');
-    features = files.filter(file => fs.readFileSync(file, 'utf8').includes(args.cucumberOpts.tags));
+    features = files.filter(file => fs.readFileSync(file, 'utf8').includes(args.cucumberOpts.tagExpression));
   } else {
     features = './test/features/*.feature';
   }
