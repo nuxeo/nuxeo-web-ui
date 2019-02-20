c50fcdcd961c545cf00580e4ffe2f9091b8fb943
Gabriel Barata <gbarata@nuxeo.com>
2019-02-20 19:06:13 +0000

    NXP-26766: add cropper stylesheet and jsondiffpatch as external dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -153,6 +153,7 @@ gulp.task('move-elements', function() {
     dist('elements/elements.js'),
     dist('elements/routing.js')])
     .pipe($.replace('..\/node_modules', 'node_modules'))
+    .pipe($.replace(/(?<="|')\/node_modules/, 'node_modules'))
     .pipe($.replace('..\/fonts\/', './fonts/'))
     .pipe(gulp.dest(dist()));
 
