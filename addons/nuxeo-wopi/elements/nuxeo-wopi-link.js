c3e58bee4f579f128613cea31ead2e9eedb5eaad
Nelson Silva <nsilva@nuxeo.com>
2019-03-11 16:53:24 +0000

    NXP-26967: migrate nuxeo-wopi to polymer 3

--- addons/nuxeo-wopi/elements/nuxeo-wopi-link.js
+++ addons/nuxeo-wopi/elements/nuxeo-wopi-link.js
@@ -1,4 +1,4 @@
-<!--
+/**
 (C) Copyright 2018 Nuxeo SA (http://nuxeo.com/) and contributors.
 Licensed under the Apache License, Version 2.0 (the 'License');
 you may not use this file except in compliance with the License.
@@ -13,67 +13,63 @@ See the License for the specific language governing permissions and
 limitations under the License.
 Contributors:
   Thomas Roger <troger@nuxeo.com>
--->
+*/
+import { html } from '@polymer/polymer/lib/utils/html-tag.js';
+import { FiltersBehavior } from '@nuxeo/nuxeo-ui-elements/nuxeo-filters-behavior.js';
 
-<!--
+/**
 `nuxeo-wopi-link`
 @group Nuxeo UI
 @element nuxeo-wopi-link
--->
-<dom-module id="nuxeo-wopi-link">
-  <template>
+*/
+Polymer({
+  _template: html`
     <template is="dom-if" if="[[_isAvailable(url)]]">
       <nuxeo-link-button href="[[url]]" icon-src="[[_wopiIcon(appName)]]" label="[[_wopiTooltip(appName)]]" target="_blank"></nuxeo-link-button>
     </template>
-  </template>
+`,
 
-  <script>
-    Polymer({
-      is: 'nuxeo-wopi-link',
+  is: 'nuxeo-wopi-link',
+  behaviors: [FiltersBehavior],
 
-      behaviors: [Nuxeo.FiltersBehavior],
+  properties: {
+    document: Object,
+    blob: Object,
+    appName: {
+      type: String,
+      computed: '_appName(blob)'
+    },
+    url: {
+      type: String,
+      computed: '_wopiURL(document, blob)'
+    }
+  },
 
-      properties: {
-        document: Object,
-        blob: Object,
-        appName: {
-          type: String,
-          computed: '_appName(blob)'
-        },
-        url: {
-          type: String,
-          computed: '_wopiURL(document, blob)'
-        }
-      },
+  _appName() {
+    return this.blob
+      && this.blob.wopi
+      && this.blob.wopi.appName
+      && this.blob.wopi.appName.toLowerCase();
+  },
 
-      _appName() {
-        return this.blob
-          && this.blob.wopi
-          && this.blob.wopi.appName
-          && this.blob.wopi.appName.toLowerCase();
-      },
+  _isAvailable() {
+    return !!this.url;
+  },
 
-      _isAvailable() {
-        return !!this.url;
-      },
+  _wopiIcon() {
+    return 'images/' + this.appName + '.png'
+  },
 
-      _wopiIcon() {
-        return 'images/' + this.appName + '.png'
-      },
+  _wopiTooltip() {
+    return 'wopiLink.' + this.appName + '.tooltip';
+  },
 
-      _wopiTooltip() {
-        return 'wopiLink.' + this.appName + '.tooltip';
-      },
+  _wopiURL() {
+    var blobInfo = this.blob && this.blob.wopi;
+    if (!blobInfo) {
+      return null;
+    }
 
-      _wopiURL() {
-        var blobInfo = this.blob && this.blob.wopi;
-        if (!blobInfo) {
-          return null;
-        }
-
-        return blobInfo.edit && this.hasPermission(this.document, 'WriteProperties') ? blobInfo.edit : blobInfo.view;
-      }
-    });
-  </script>
-
-</dom-module>
+    return blobInfo.edit && this.hasPermission(this.document, 'WriteProperties') ? blobInfo.edit : blobInfo.view;
+  }
+});
