614cb124bba6ee77ef66f5e230fbf4f1e37d7187
Nelson Silva <nsilva@nuxeo.com>
2019-03-12 22:54:43 +0000

    NXP-26965: rely on s3.useDirectUpload configuration property

--- index.js
+++ index.js
@@ -1,40 +1,5 @@
 import { importHref } from '@nuxeo/nuxeo-ui-elements/import-href.js';
 
-// Nuxeo.UI namespace
-const Nuxeo = window.Nuxeo = window.Nuxeo || {};
-Nuxeo.UI = Nuxeo.UI || {};
-
-// default configuration
-Nuxeo.UI.config = Nuxeo.UI.config || {
-  fetch: {
-    document: ['properties']
-  },
-  enrichers: {
-    document: [
-      'hasContent',
-      'firstAccessibleAncestor',
-      'permissions',
-      'breadcrumb',
-      'preview',
-      'favorites',
-      'subscribedNotifications',
-      'thumbnail',
-      'renditions',
-      'pendingTasks',
-      'runnableWorkflows',
-      'runningWorkflows',
-      'collections',
-      'audit',
-      'subtypes',
-      'tags',
-      'publications'
-    ],
-    blob: ['appLinks']
-  },
-  dateFormat: 'LL',
-  dateTimeFormat: 'LLL'
-};
-
 // load app
 import './elements/nuxeo-app.js';
 
