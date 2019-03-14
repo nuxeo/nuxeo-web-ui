45b915526d8e7bf62ce52692d3bd06ff0c0cd076
Gabriel Barata <gbarata@nuxeo.com>
2019-03-14 15:24:20 +0000

    NXP-26978: cleanup style modules and icon sets

--- themes/base.js
+++ themes/base.js
@@ -1,8 +1,30 @@
+/**
+@license
+(C) Copyright Nuxeo Corp. (http://nuxeo.com/)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+/*
+  FIXME(polymer-modulizer): the above comments were extracted
+  from HTML and may be out of place here. Review them and
+  then delete this comment!
+*/
 import '@polymer/polymer/polymer-legacy.js';
 import '@polymer/iron-flex-layout/iron-flex-layout-classes.js';
-const $_documentContainer = document.createElement('template');
+import { html } from '@polymer/polymer/lib/utils/html-tag.js';
 
-$_documentContainer.innerHTML = `<dom-module id="nuxeo-styles">
+const template = html`
+<dom-module id="nuxeo-styles">
   <template>
     <style include="iron-positioning">
       /* headings */
@@ -537,29 +559,7 @@ $_documentContainer.innerHTML = `<dom-module id="nuxeo-styles">
     }
   }
 </style>
-</custom-style>`;
+</custom-style>
+`;
 
-document.head.appendChild($_documentContainer.content);
-
-/**
-@license
-(C) Copyright Nuxeo Corp. (http://nuxeo.com/)
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-/*
-  FIXME(polymer-modulizer): the above comments were extracted
-  from HTML and may be out of place here. Review them and
-  then delete this comment!
-*/
-;
+document.head.appendChild(template.content);
