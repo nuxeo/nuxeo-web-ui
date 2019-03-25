109c352a697280381fd7ee2223b76981bc84c7bd
Nelson Silva <nsilva@nuxeo.com>
2019-03-25 18:39:24 +0000

    NXP-27070: migrate nuxeo-imap-connector to polymer 3

--- addons/nuxeo-imap-connector/elements/nuxeo-mail-sync-button.js
+++ addons/nuxeo-imap-connector/elements/nuxeo-mail-sync-button.js
@@ -1,4 +1,4 @@
-<!--
+/*
 (C) Copyright 2018 Nuxeo (http://nuxeo.com/) and contributors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,44 +12,43 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
--->
-<link rel="import" href="nuxeo-mail-icons.html">
+*/
+import { Polymer } from '@polymer/polymer/lib/legacy/polymer-fn.js';
+import { html } from '@polymer/polymer/lib/utils/html-tag.js';
+import { I18nBehavior } from '@nuxeo/nuxeo-ui-elements/nuxeo-i18n-behavior.js';
+import './nuxeo-mail-icons.js';
 
-<!--
+/**
 `nuxeo-mail-sync-button`
 @group Nuxeo UI
 @element nuxeo-mail-sync-button
--->
-<dom-module id="nuxeo-mail-sync-button">
-  <template>
-    <nuxeo-operation-button operation="Mail.CheckInbox" input="[[document.uid]]" event="document-updated"
-                            notification="[[i18n('mailfolder.sync.success')]]"
-                            icon="nuxeo-mail:sync" label="[[i18n('mailfolder.sync')]]" on-tap="_showNotification"></nuxeo-operation-button>
+*/
+Polymer({
+  _template: html`
+    <nuxeo-operation-button operation="Mail.CheckInbox" input="[[document.uid]]" event="document-updated" notification="[[i18n('mailfolder.sync.success')]]" icon="nuxeo-mail:sync" label="[[i18n('mailfolder.sync')]]" on-tap="_showNotification"></nuxeo-operation-button>
 
     <paper-toast id="toast" text="[[i18n('mailfolder.sync.checking')]]" duration="0"></paper-toast>
-  </template>
-  <script>
-    Polymer({
-      is: 'nuxeo-mail-sync-button',
-      behaviors: [Nuxeo.I18nBehavior],
-      properties: {
-        /**
-         * Input document
-         */
-        document: Object
-      },
+`,
 
-      listeners: {
-        'document-updated': '_closeNotification'
-      },
+  is: 'nuxeo-mail-sync-button',
+  behaviors: [I18nBehavior],
 
-      _showNotification: function() {
-        this.$.toast.open();
-      },
+  properties: {
+    /**
+     * Input document
+     */
+    document: Object
+  },
 
-      _closeNotification: function() {
-        this.$.toast.hide();
-      }
-    });
-  </script>
-</dom-module>
+  listeners: {
+    'document-updated': '_closeNotification'
+  },
+
+  _showNotification: function() {
+    this.$.toast.open();
+  },
+
+  _closeNotification: function() {
+    this.$.toast.hide();
+  }
+});
--- addons/nuxeo-imap-connector/index.js
+++ addons/nuxeo-imap-connector/index.js
@@ -1,5 +1,6 @@
-<!--
-(C) Copyright 2018 Nuxeo (http://nuxeo.com/) and others.
+/**
+@license
+(C) Copyright Nuxeo Corp. (http://nuxeo.com/)
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -12,13 +13,14 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
--->
-<nuxeo-slot-content name="syncEmailAction" slot="DOCUMENT_ACTIONS">
-  <template>
-    <nuxeo-filter document="[[document]]" type="MailFolder" permission="ReadWrite">
-      <template>
-        <nuxeo-mail-sync-button document="[[document]]"></nuxeo-mail-sync-button>
-      </template>
-    </nuxeo-filter>
-  </template>
-</nuxeo-slot-content>
+*/
+import './elements/nuxeo-mail-sync-button.js';
+
+import html from './nuxeo-imap-connector.html';
+
+const tmpl = document.createElement('template');
+tmpl.innerHTML = html;
+document.head.appendChild(tmpl.content);
+
+// push MailFolder to list of document types that are not available for import
+window.nuxeo.importBlacklist.push('MailFolder');
