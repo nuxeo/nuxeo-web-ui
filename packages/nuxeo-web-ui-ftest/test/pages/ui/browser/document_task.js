a9f5af000257b5e53004a414f6047717c5460d0d
Nuno Cunha <ncunha@nuxeo.com>
2019-05-24 16:59:32 +0100

    NXP-27177: correct ftests to get actor name

--- packages/nuxeo-web-ui-ftest/test/pages/ui/browser/document_task.js
+++ packages/nuxeo-web-ui-ftest/test/pages/ui/browser/document_task.js
@@ -38,7 +38,7 @@ export default class DocumentTask extends BasePage {
 
   actorExists(element, actor) {
     const users = element.elements('nuxeo-user-tag .tag a').value;
-    return users.some(user => user.getText() === `${actor} undefined`);
+    return users.some(user => user.getText() === `${actor}`);
   }
 
   performAction(name) {
