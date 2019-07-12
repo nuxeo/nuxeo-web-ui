6ab26044695632b3c8ec2eb52042b7df59f8be85
Nuno Cunha <ncunha@nuxeo.com>
2019-07-12 17:42:39 +0100

    NXP-26640: easyshare sharing dialog improvements

--- addons/easyshare/elements/nuxeo-easyshare-share-link.js
+++ addons/easyshare/elements/nuxeo-easyshare-share-link.js
@@ -17,6 +17,8 @@ limitations under the License.
 import { Polymer } from '@polymer/polymer/lib/legacy/polymer-fn.js';
 import { html } from '@polymer/polymer/lib/utils/html-tag.js';
 import { I18nBehavior } from '@nuxeo/nuxeo-ui-elements/nuxeo-i18n-behavior.js';
+import { Debouncer } from '@polymer/polymer/lib/utils/debounce.js';
+import { timeOut } from '@polymer/polymer/lib/utils/async.js';
 
 /**
 `nuxeo-easyshare-share-link`
@@ -30,10 +32,30 @@ Polymer({
         display: inline-block;
       }
 
-      .heading {
+      .horizontal {
         @apply --layout-horizontal;
         @apply --layout-center;
         @apply --layout-justified;
+      }
+
+      .selected {
+        color: var(--nuxeo-primary-color, #0066ff);
+        pointer-events: none;
+      }
+
+      iron-icon {
+        cursor: pointer;
+        margin-left: 10px;
+      }
+
+      iron-icon:hover {
+        color: var(--nuxeo-primary-color, #0066ff);
+      }
+
+      nuxeo-input {
+        cursor: text;
+        overflow: hidden;
+        @apply --layout-flex;
       }
     </style>
     <nuxeo-connection id="nxcon"></nuxeo-connection>
@@ -41,33 +63,39 @@ Polymer({
     <template is="dom-if" if="[[_isAvailable(document)]]">
       <div class="action" on-tap="_toggleDialog">
         <paper-icon-button id="shareBtn" icon="[[icon]]" noink></paper-icon-button>
-        <span class="label" hidden$="[[!showLabel]]">[[i18n('shareButton.tooltip')]]</span>
+        <span class="label" hidden$="[[!showLabel]]">[[_label]]</span>
       </div>
-      <nuxeo-tooltip for="shareBtn">[[i18n('shareButton.tooltip')]]</nuxeo-tooltip>
+      <nuxeo-tooltip for="shareBtn">[[_label]]</nuxeo-tooltip>
     </template>
 
     <nuxeo-dialog id="dialog" with-backdrop>
-      <div class="heading">
+      <div>
         <h2>[[i18n('shareButton.dialog.heading')]]</h2>
       </div>
-      <nuxeo-input
-        id="permalink"
-        label="[[i18n('easyshare.copy.label', document.properties.dc:title)]]"
-        value="[[_buildPermalink(document)]]"
-        autofocus
-        readonly
-      >
-      </nuxeo-input>
-
-      <template is="dom-if" if="[[_isEasyshare(document)]]">
+      <div id="permanent" class="horizontal">
         <nuxeo-input
-          id="easyshareLink"
-          label="[[i18n('easysharefolder.share', document.properties.dc:title)]]"
-          value="[[_buildEasysharelink(document)]]"
-          autofocus
+          id="permalink"
+          label="[[i18n('easyshare.copy.label', document.properties.dc:title)]]"
+          value="[[_buildPermalink(document)]]"
           readonly
         >
         </nuxeo-input>
+        <iron-icon id="permalinkIcon" name="permalinkIcon" icon="link" on-tap="_copyLink"></iron-icon>
+        <nuxeo-tooltip id="tooltip" for="permalinkIcon">[[i18n('shareButton.operation.copy')]]</nuxeo-tooltip>
+      </div>
+
+      <template is="dom-if" if="[[_isEasyshare(document)]]">
+        <div id="easyShare" class="horizontal">
+          <nuxeo-input
+            id="easyShareLink"
+            label="[[i18n('easysharefolder.share', document.properties.dc:title)]]"
+            value="[[_buildEasysharelink(document)]]"
+            readonly
+          >
+          </nuxeo-input>
+          <iron-icon id="easyShareIcon" name="easyShareIcon" icon="link" on-tap="_copyLink"></iron-icon>
+          <nuxeo-tooltip id="tooltip" for="easyShareIcon">[[i18n('shareButton.operation.copy')]]</nuxeo-tooltip>
+        </div>
       </template>
       <div class="buttons">
         <paper-button dialog-dismiss>[[i18n('shareButton.dialog.close')]]</paper-button>
@@ -101,11 +129,15 @@ Polymer({
       type: Boolean,
       value: false,
     },
+
+    _label: {
+      type: String,
+      computed: '_computeLabel(i18n)',
+    },
   },
 
   _toggleDialog() {
     this.$.dialog.toggle();
-    this.$.permalink.inputElement.inputElement.select();
   },
 
   _isAvailable(document) {
@@ -123,5 +155,43 @@ Polymer({
   _buildEasysharelink(document) {
     const baseUrl = window.location.origin + this.$.nxcon.url;
     return document ? `${baseUrl}/site/easyshare/${this.document.uid}` : '';
+  },
+
+  _computeLabel() {
+    return this.i18n('shareButton.tooltip');
+  },
+
+  _copyLink(e) {
+    const shareButton = e.currentTarget;
+    const link = shareButton.previousElementSibling;
+
+    const otherShareButton = shareButton.id === 'easyShareIcon' ? this.$.permalinkIcon : this.$$('#easyShareIcon');
+    if (
+      otherShareButton &&
+      otherShareButton.display !== 'none' &&
+      otherShareButton._debouncer &&
+      otherShareButton._debouncer.isActive()
+    ) {
+      otherShareButton._debouncer = otherShareButton._debouncer.flush();
+    }
+
+    // Select Link
+    link.$.paperInput.$.nativeInput.select();
+    if (!window.document.execCommand('copy')) {
+      return;
+    }
+
+    shareButton._debouncer = Debouncer.debounce(shareButton._debouncer, timeOut.after(2000), () => {
+      // Unselect Link
+      link.$.paperInput.$.nativeInput.setSelectionRange(0, 0);
+      link.$.paperInput.blur();
+
+      shareButton.set('icon', 'link');
+      shareButton.classList.remove('selected');
+    });
+
+    shareButton.set('icon', 'check');
+    shareButton.classList.add('selected');
+    this.fire('notify', { message: this.i18n('shareButton.operation.copied'), duration: 2000 });
   },
 });
