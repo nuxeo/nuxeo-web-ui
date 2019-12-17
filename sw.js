bd7e0f70e6985b65b91c3609a208e454ff3d8574
Nelson Silva <nsilva@nuxeo.com>
2019-12-17 14:28:39 +0000

    NXP-28431: move fetch event handlers outside install callback

--- sw.js
+++ sw.js
@@ -3,15 +3,15 @@
 importScripts('workbox/workbox-sw.js');
 workbox.loadModule('workbox-strategies');
 
+const params = new URL(self.location.href).searchParams;
+if (params.has('ts')) {
+  workbox.routing.registerRoute(/\.*\.(html|js)$/, async ({ url, event }) => {
+    const strategy = workbox.strategies.networkFirst();
+    const request = new Request(`${url}?ts=${params.get('ts')}`, { credentials: 'same-origin' });
+    return await strategy.makeRequest({ event, request });
+  });
+}
+
 self.addEventListener('install', () => {
   self.skipWaiting();
-
-  const params = new URL(self.location.href).searchParams;
-  if (params.has('ts')) {
-    workbox.routing.registerRoute(/\.*\.(html|js)$/, async ({ url, event }) => {
-      const strategy = workbox.strategies.networkFirst();
-      const request = new Request(`${url}?ts=${params.get('ts')}`, { credentials: 'same-origin' });
-      return await strategy.makeRequest({ event, request });
-    });
-  }
 });
