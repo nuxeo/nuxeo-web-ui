87e19b0adc1d1261828ce05a6ffd3c2896109452
Nuno Cunha <ncunha@nuxeo.com>
2020-02-17 12:49:13 +0000

    NXP-26059: ftest for clipboard move and recents update

--- packages/nuxeo-web-ui-ftest/pages/ui/recents.js
+++ packages/nuxeo-web-ui-ftest/pages/ui/recents.js
@@ -11,4 +11,30 @@ export default class Recents extends BasePage {
     });
     return count;
   }
+
+  select(name) {
+    const entries = this.el.elements('#recentDocumentsList .list-item-title').value;
+    const doc = entries.find((entry) => entry.getText().trim() === name);
+    if (doc) {
+      doc.click();
+      return true;
+    }
+    return false;
+  }
+
+  waitForHasMember(title, reverse) {
+    driver.waitUntil(
+      () => {
+        const members = this.el.elements('#recentDocumentsList .list-item-title').value;
+        if (reverse) {
+          return members.every((member) => member.getText().trim() !== title);
+        }
+        return members.some((member) => member.getText().trim() === title);
+      },
+      reverse
+        ? 'There is such member in the recently viewed list'
+        : 'There is no such member in the recently viewed list',
+    );
+    return true;
+  }
 }
