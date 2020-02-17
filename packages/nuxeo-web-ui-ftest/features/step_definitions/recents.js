a93e659693b2a8a05b0cbf152792e59ae0d57db3
Nuno Cunha <ncunha@nuxeo.com>
2020-02-17 12:49:13 +0000

    NXP-26059: ftest for clipboard move and recents update

--- packages/nuxeo-web-ui-ftest/features/step_definitions/recents.js
+++ packages/nuxeo-web-ui-ftest/features/step_definitions/recents.js
@@ -1,4 +1,10 @@
-import { Then } from 'cucumber';
+import { Then, When } from 'cucumber';
+
+When('I can click on recently viewed documents item {string}', function(title) {
+  this.ui.drawer.recents.waitForVisible();
+  this.ui.drawer.recents.waitForHasMember(title).should.be.true;
+  this.ui.drawer.recents.select(title).should.be.true;
+});
 
 Then('I can see the list of recently viewed documents', function() {
   this.ui.drawer.recents.waitForVisible().should.be.true;
@@ -11,5 +17,5 @@ Then('I can see the list of recently viewed documents has {int} item(s)', functi
 
 Then('I can see the list of recently viewed documents has {string} document', function(title) {
   this.ui.drawer.recents.waitForVisible();
-  this.ui.drawer.recents.el.hasElementByTextContent('#recentDocumentsList .list-item-title', title).should.be.true;
+  this.ui.drawer.recents.waitForHasMember(title).should.be.true;
 });
