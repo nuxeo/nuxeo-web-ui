98f045d74b48c4e679e8e4eb83369511f6dacc3d
Nelson Silva <nsilva@nuxeo.com>
2020-03-10 14:52:07 +0000

    NXP-28448: generate cucumber report

--- scripts/test-runner.js
+++ scripts/test-runner.js
@@ -3,13 +3,16 @@ const fs = require('fs');
 const path = require('path');
 const args = require('minimist')(process.argv.slice(2));
 
+const FTEST = path.join(__dirname, '../ftest');
+
 function runFunctionalTests(project, dir) {
   if (!fs.existsSync(dir)) {
     console.warn(`The directory "${dir}" doesn't exist, no tests will run for "${project}".`);
     return;
   }
   console.info(`Starting Functional Tests for "${project}" on "${dir}" location.`);
-  const run = spawnSync('nuxeo-web-ui-ftest', process.argv, { cwd: dir, stdio: 'inherit' });
+  const ftestArgs = [...process.argv, '--cucumberReport', path.join(FTEST, 'target/cucumber-reports')];
+  const run = spawnSync('nuxeo-web-ui-ftest', ftestArgs, { cwd: dir, stdio: 'inherit' });
   if (run.status !== 0) {
     console.error(`An error was returned by the process running the Functional Tests for "${project}".`);
     process.exit(run.status);
@@ -17,7 +20,7 @@ function runFunctionalTests(project, dir) {
 }
 
 if (!args.skipWebUi) {
-  runFunctionalTests('WebUI', path.join(__dirname, `../ftest`));
+  runFunctionalTests('WebUI', FTEST);
 }
 (process.env.NUXEO_PACKAGES || '')
   .split(/[\s,]+/)
