caee87c5e44ab62689f6f57bd69e2f9bf8fd04f8
Pedro Goncalves <pgoncalves@nuxeo.com>
2020-03-25 22:26:48 +0000

    NXP-27969: automate video dam tests

---
+++ addons/nuxeo-dam/ftest/features/step_definitions/video.js
@@ -0,0 +1,17 @@
+import { Then } from 'cucumber';
+
+Then('I can see the video conversions panel', function() {
+  const page = this.ui.browser.documentPage(this.doc.type);
+  page.waitForVisible();
+  page.el.element('nuxeo-video-conversions').waitForVisible().should.be.true;
+});
+
+Then('I can see the video storyboard', function() {
+  const page = this.ui.browser.documentPage(this.doc.type);
+  page.waitForVisible();
+  const videoViewer = page.el.element('nuxeo-video-viewer');
+  videoViewer.waitForVisible();
+  videoViewer.element('#storyboard').waitForVisible().should.be.true;
+});
+
+
