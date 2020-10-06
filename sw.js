3f1d6d0aa66f7c957ec07ae5ad0fb922f47c467b
Nelson Silva <nsilva@nuxeo.com>
2020-10-06 12:44:26 +0100

    WEBUI-86: bypass service worker for API requests

--- sw.js
+++ sw.js
@@ -3,11 +3,13 @@ workbox.loadModule('workbox-strategies');
 
 const params = new URL(self.location.href).searchParams;
 if (params.has('ts')) {
-  workbox.routing.registerRoute(/\.*\.(html|js)$/, async ({ url, event }) => {
-    const strategy = workbox.strategies.networkFirst();
-    const request = new Request(`${url}?ts=${params.get('ts')}`, { credentials: 'same-origin' });
-    return await strategy.makeRequest({ event, request });
-  });
+  workbox.routing.registerRoute(
+    ({ url, event }) => /\.*\.(html|js)$/.test(url) && event.request.headers.get('Content-Type') !== 'application/json',
+    async ({ url, event }) => {
+      const strategy = workbox.strategies.networkFirst();
+      const request = new Request(`${url}?ts=${params.get('ts')}`, { credentials: 'same-origin' });
+      return await strategy.makeRequest({ event, request });
+    });
 }
 
 self.addEventListener('install', () => {
