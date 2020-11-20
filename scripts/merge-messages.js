0ec8aef41181be7ddabf721de2e8eef01892095d
Nelson Silva <nsilva@nuxeo.com>
2020-11-20 13:14:48 +0000

    WEBUI-133: improve dev flow for addons

--- scripts/merge-messages.js
+++ scripts/merge-messages.js
@@ -1,20 +1,31 @@
 const glob = require('glob');
-const fs = require('fs');
+const { mkdirSync, existsSync, writeFileSync } = require('fs');
 const path = require('path');
 
 const DEST = '.tmp/i18n';
 const CWD = process.cwd();
 
-fs.mkdirSync(DEST, { recursive: true });
+mkdirSync(DEST, { recursive: true });
+
+const BUNDLES = (process.env.NUXEO_PACKAGES || '')
+  .split(/[\s,]+/)
+  .filter(Boolean)
+  .filter((p) => existsSync(`addons/${p}`));
+
+const SOURCES = [...BUNDLES.map((b) => `addons/${b}/i18n`), 'node_modules/@nuxeo/nuxeo-ui-elements/i18n'];
 
 glob('i18n/messages*.json', (_, files) =>
   files.forEach((file) => {
     const messages = require(`${CWD}/${file}`);
     const filename = path.basename(file);
-    const p = `${CWD}/node_modules/@nuxeo/nuxeo-ui-elements/i18n/${filename}`;
-    if (fs.existsSync(p)) {
-      Object.assign(messages, require(p));
-    }
-    fs.writeFileSync(`${DEST}/${filename}`, JSON.stringify(messages, null, 2));
+
+    SOURCES.forEach((s) => {
+      const p = `${CWD}/${s}/${filename}`;
+      if (existsSync(p)) {
+        Object.assign(messages, require(p));
+      }
+    });
+
+    writeFileSync(`${DEST}/${filename}`, JSON.stringify(messages, null, 2));
   }),
 );
