3611b616c1ec5698a992c69834871349fd5c653d
Nelson Silva <nsilva@nuxeo.com>
2021-03-24 13:26:26 +0000

    WEBUI-278: allow getting hidden field values for ftest

--- packages/nuxeo-web-ui-ftest/pages/ui/browser/document_layout.js
+++ packages/nuxeo-web-ui-ftest/pages/ui/browser/document_layout.js
@@ -3,13 +3,11 @@ import BasePage from '../../base';
 export default class DocumentLayout extends BasePage {
   getField(field) {
     driver.waitForExist(this._selector);
-    driver.waitForVisible(this._selector);
     return this.el.$(`[name="${field}"]`);
   }
 
   getFieldValue(field) {
     const fieldEl = this.getField(field);
-    fieldEl.waitForVisible();
     return fixtures.layouts.getValue(fieldEl);
   }
 
