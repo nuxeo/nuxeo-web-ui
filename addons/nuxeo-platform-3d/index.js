7aefd5166bbb981aa3db3cbd322dd13784754b1e
Miguel Nixo <mnixo@nuxeo.com>
2021-05-21 17:07:53 +0100

    WEBUI-237: import threejs v0.81.0 utilities as ESM
    Co-authored-by: Nelson Silva <nsilva@nuxeo.com>

--- addons/nuxeo-platform-3d/index.js
+++ addons/nuxeo-platform-3d/index.js
@@ -14,6 +14,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+import './controls/OrbitControls.js';
 import './elements/nuxeo-3d-viewer.js';
 import './elements/nuxeo-3d-preview.js';
 import './elements/nuxeo-3d-render-views.js';
