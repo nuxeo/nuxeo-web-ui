f1d31a9898692cb4053ac1f8cb488ce026487a7e
Ricardo Dias <rdias@nuxeo.com>
2021-06-21 18:03:08 +0100

    WEBUI-296: improve notification handling in Web UI for aborted actions

--- elements/nuxeo-app.js
+++ elements/nuxeo-app.js
@@ -1352,7 +1352,7 @@ Polymer({
 
   _getToastFor(source, data) {
     let { toast } = this.$;
-    const { callback, dismissible } = data;
+    const { abort, dismissible } = data;
     if (!source) {
       toast.addEventListener('MDCSnackbar:opening', () => {
         // HACK - to position the snackbar and style the internal label
@@ -1382,10 +1382,10 @@ Polymer({
         });
         // listen to the closed event to track dismiss and custom action
         toast.addEventListener('MDCSnackbar:closed', (e) => {
-          if (e.detail.reason === 'action' && callback) {
-            callback();
-            localStorage.setItem(id, JSON.stringify({ dismissed: false }));
-          } else if (e.detail.reason === 'dismiss') {
+          if (e.detail.reason === 'action' && abort) {
+            localStorage.setItem(id, JSON.stringify({ dismissed: false, aborted: true }));
+            abort();
+          } else if (e.detail.reason === 'dismiss') {
             const state = JSON.parse(localStorage.getItem(id));
             if (state && state.ended) {
               localStorage.removeItem(id);
@@ -1413,13 +1413,16 @@ Polymer({
     if (message) {
       // if the toast was dismissed, then we shouldn't display it until the action ends
       const state = JSON.parse(localStorage.getItem(toast.id));
-      if (state && state.dismissed && abort) {
+      if (state && (state.dismissed || state.aborted) && abort) {
         return;
       }
       if (state && !abort) {
-        localStorage.setItem(toast.id, JSON.stringify({
-          ended: true,
-        }));
+        localStorage.setItem(
+          toast.id,
+          JSON.stringify({
+            ended: true,
+          }),
+        );
       }
 
       // update the snackbar properties
