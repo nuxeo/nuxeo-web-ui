1eecfdc13257005fe8b3acaf160f517eaa8833d8
Ricardo Dias <rdias@nuxeo.com>
2021-07-15 15:17:51 +0100

    WEBUI-295: make bulk action buttons extend nuxeo-operation-button

--- elements/nuxeo-document-bulk-actions/nuxeo-untrash-documents-button.js
+++ elements/nuxeo-document-bulk-actions/nuxeo-untrash-documents-button.js
@@ -1,20 +1,19 @@
 /**
-@license
-(C) Copyright Nuxeo Corp. (http://nuxeo.com/)
+ @license
+ (C) Copyright Nuxeo Corp. (http://nuxeo.com/)
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+ http://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-import '@polymer/polymer/polymer-legacy.js';
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
 
 import '@polymer/paper-icon-button/paper-icon-button.js';
 import '@nuxeo/nuxeo-elements/nuxeo-operation.js';
@@ -23,83 +22,69 @@ import '@nuxeo/nuxeo-ui-elements/nuxeo-icons.js';
 import { I18nBehavior } from '@nuxeo/nuxeo-ui-elements/nuxeo-i18n-behavior.js';
 import { FiltersBehavior } from '@nuxeo/nuxeo-ui-elements/nuxeo-filters-behavior.js';
 import '@nuxeo/nuxeo-ui-elements/widgets/nuxeo-tooltip.js';
-import { Polymer } from '@polymer/polymer/lib/legacy/polymer-fn.js';
 import { html } from '@polymer/polymer/lib/utils/html-tag.js';
+import { mixinBehaviors } from '@polymer/polymer/lib/legacy/class';
 import { SelectAllBehavior } from '../nuxeo-select-all-behavior.js';
 
 /**
-`nuxeo-untrash-documents-actions`
-@group Nuxeo UI
-@element nuxeo-untrash-documents-actions
-*/
-Polymer({
-  _template: html`
-    <style include="nuxeo-action-button-styles nuxeo-styles"></style>
+ `nuxeo-untrash-documents-actions`
+ @group Nuxeo UI
+ @element nuxeo-untrash-documents-actions
+ */
+class NuxeoUntrashDocumentsButton extends mixinBehaviors(
+  [SelectAllBehavior, I18nBehavior, FiltersBehavior],
+  Nuxeo.OperationButton,
+) {
+  static get template() {
+    return html`
+      <style include="nuxeo-action-button-styles nuxeo-styles"></style>
+      ${super.template}
+    `;
+  }
 
-    <nuxeo-operation-button
-      id="untrashAllButton"
-      icon="nuxeo:restore-deleted"
-      input="[[view]]"
-      label="[[_label]]"
-      operation="Document.Untrash"
-      params="[[_params()]]"
-      show-label="[[showLabel]]"
-      tooltip-position="[[tooltipPosition]]"
-      sync-indexing
-      hidden="[[!_isAvailable(documents.splices)]]"
-    >
-    </nuxeo-operation-button>
-  `,
+  static get is() {
+    return 'nuxeo-untrash-documents-button';
+  }
 
-  is: 'nuxeo-untrash-documents-button',
-  behaviors: [SelectAllBehavior, I18nBehavior, FiltersBehavior],
+  static get properties() {
+    return {
+      documents: {
+        type: Array,
+        notify: true,
+        value: [],
+      },
 
-  properties: {
-    documents: {
-      type: Array,
-      notify: true,
-      value: [],
-    },
+      icon: {
+        type: String,
+        value: 'nuxeo:restore-deleted',
+      },
+    };
+  }
 
-    tooltipPosition: {
-      type: String,
-      value: 'bottom',
-    },
+  static get observers() {
+    return ['_isVisible(documents.splices)', '_updateLabel(i18n)'];
+  }
 
-    /**
-     * `true` if the action should display the label, `false` otherwise.
-     */
-    showLabel: {
-      type: Boolean,
-      value: false,
-    },
-
-    _label: {
-      type: String,
-      computed: '_computeLabel(i18n)',
-    },
-  },
-
-  attached() {
-    // capture the click event on the capture phase to set nuxeo-operation-buttons properties
-    this._untrashDocumentsListener = this._untrashDocuments.bind(this);
-    this.$.untrashAllButton.addEventListener('click', this._untrashDocumentsListener, { capture: true });
-  },
-
-  detached() {
-    this.$.untrashAllButton.removeEventListener('click', this._untrashDocumentsListener);
-  },
+  _execute() {
+    this.untrashDocuments();
+  }
 
   untrashDocuments() {
     if (
       (this._isSelectAllActive() || this.docsHavePermissions) &&
       window.confirm(this.i18n('untrashDocumentsButton.confirm.untrashDocuments'))
     ) {
-      const {documents} = this;
+      const { documents } = this;
       const isSelectAllActive = this._isSelectAllActive();
       // if select all is active, then we don't pass the documents (we untrash all of them)
       const detail = isSelectAllActive ? {} : { documents };
-      this.$.untrashAllButton
+
+      this.input = this.view;
+      this.operation = 'Document.Untrash';
+      this.params = this._params();
+      this.syncIndexing = true;
+
+      super
         ._execute()
         .then(() => {
           this.fire('nuxeo-documents-untrashed', detail);
@@ -112,43 +97,42 @@ Polymer({
           }
         });
     }
-  },
-
-  _untrashDocuments(e) {
-    // we cannot trigger the nuxeo-operation-button directly, because we need the user confirmation first
-    e.preventDefault();
-    e.stopPropagation();
-    this.untrashDocuments();
-  },
+  }
 
   _params() {
     return {};
-  },
+  }
+
+  _isVisible() {
+    this.hidden = !this._isAvailable();
+  }
 
   _isAvailable() {
     return (
       this._isSelectAllActive() ||
       (this.documents && this.documents.length > 0 && this._checkDocsPermissions() && this._checkDocsAreTrashed())
     );
-  },
+  }
 
   _checkDocsAreTrashed() {
     return this.documents.every((document) => this.isTrashed(document));
-  },
+  }
 
   _checkDocsPermissions() {
     this.docsHavePermissions = !this.documents.some((document) => !this._docHasPermissions(document));
     return this.docsHavePermissions;
-  },
+  }
 
   /*
    * Checks if a single given document has 'Write' permission
    */
   _docHasPermissions(document) {
     return this.hasPermission(document, 'Write');
-  },
+  }
 
-  _computeLabel() {
-    return this.i18n('untrashDocumentsButton.tooltip');
-  },
-});
+  _updateLabel() {
+    this.label = this.i18n('untrashDocumentsButton.tooltip');
+  }
+}
+
+window.customElements.define(NuxeoUntrashDocumentsButton.is, NuxeoUntrashDocumentsButton);
