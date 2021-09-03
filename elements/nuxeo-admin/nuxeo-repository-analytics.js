8192ee4236f53800625c09bba662565495c73495
Andrea Cornaglia <acornaglia@nuxeo.com>
2021-09-03 18:15:19 +0100

    WEBUI-251: remove options from pie chart since equal to default

--- elements/nuxeo-admin/nuxeo-repository-analytics.js
+++ elements/nuxeo-admin/nuxeo-repository-analytics.js
@@ -165,12 +165,7 @@ Polymer({
         </nuxeo-repository-data>
 
         <nuxeo-card heading="[[i18n('repositoryAnalytics.documentTypes.heading')]]">
-          <chart-pie
-            values="[[_values(typeCount)]]"
-            labels="[[_labels(typeCount)]]"
-            options='{ "legend": { "display": true, "position": "bottom", "labels": { "boxWidth": 12 } }, "animation": false }'
-          >
-          </chart-pie>
+          <chart-pie values="[[_values(typeCount)]]" labels="[[_labels(typeCount)]]"> </chart-pie>
         </nuxeo-card>
 
         <!-- Top 10 creators -->
@@ -185,12 +180,7 @@ Polymer({
         </nuxeo-repository-data>
 
         <nuxeo-card heading="[[i18n('repositoryAnalytics.topNCreators.heading', '10')]]">
-          <chart-pie
-            values="[[_values(topCreators)]]"
-            labels="[[_labels(topCreators)]]"
-            options='{ "legend": { "display": true, "position": "bottom", "labels": { "boxWidth": 12 } }, "animation": false }'
-          >
-          </chart-pie>
+          <chart-pie values="[[_values(topCreators)]]" labels="[[_labels(topCreators)]]"> </chart-pie>
         </nuxeo-card>
 
         <!-- Documents created per week -->
@@ -244,12 +234,7 @@ Polymer({
         </nuxeo-repository-data>
 
         <nuxeo-card heading="[[i18n('repositoryAnalytics.filesByMimeType.heading')]]">
-          <chart-pie
-            values="[[_values(filesByMimeType)]]"
-            labels="[[_types(filesByMimeType)]]"
-            options='{ "legend": { "display": true, "position": "bottom", "labels": { "boxWidth": 12 } }, "animation": false }'
-          >
-          </chart-pie>
+          <chart-pie values="[[_values(filesByMimeType)]]" labels="[[_types(filesByMimeType)]]"> </chart-pie>
         </nuxeo-card>
       </div>
     </template>
