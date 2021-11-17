9f8f6b125f6dc206a95854be8c1d1a7dae81691c
Gabriel Barata <gbarata@nuxeo.com>
2021-11-17 18:25:52 +0000

    WEBUI-265: test Nuxeo Browser

--- plugin/a11y/test/helpers/login.js
+++ plugin/a11y/test/helpers/login.js
@@ -1,10 +1,13 @@
 import Login from '@nuxeo/nuxeo-web-ui-ftest/pages/login';
+import UI from '@nuxeo/nuxeo-web-ui-ftest/pages/ui';
 
 const login = (username = 'Administrator', password = 'Administrator') => {
   const logIn = Login.get();
   logIn.username = username;
   logIn.password = password;
   logIn.submit();
+  const ui = UI.get();
+  ui.waitForVisible('nuxeo-page');
 };
 
 export default login;
---
+++ plugin/a11y/test/specs/browser.js
@@ -0,0 +1,45 @@
+import '../imports';
+import UI from '@nuxeo/nuxeo-web-ui-ftest/pages/ui';
+import documentService from '@nuxeo/nuxeo-web-ui-ftest/features/step_definitions/support/services/documentService';
+import login from '../helpers/login';
+import { reportA11y } from '../a11y-reporter.js';
+
+const EXPECTED_VIOLATIONS = {
+  'aria-command-name': 1,
+  'aria-roles': 3,
+  'aria-tooltip-name': 1,
+  'duplicate-id': 28,
+  'landmark-one-main': 1,
+  'meta-viewport': 1,
+  'page-has-heading-one': 1,
+  region: 32,
+  'nested-interactive': 13,
+};
+
+const EXPECTED_INCOMPLETE_VIOLATIONS = {
+  'aria-allowed-attr': 1,
+  'aria-allowed-role': 7,
+  'aria-valid-attr-value': 1,
+  'color-contrast': 0,
+};
+
+describe('Nuxeo Browser', () => {
+  let doc;
+
+  before(async () => {
+    let parent = documentService.init('Workspace', 'My Workspace');
+    parent = await documentService.create('/default-domain/workspaces', parent);
+    let child = documentService.init();
+    child = await documentService.create(parent.path, child);
+    doc = child;
+  });
+
+  after(async () => documentService.reset());
+
+  reportA11y(EXPECTED_VIOLATIONS, EXPECTED_INCOMPLETE_VIOLATIONS, () => {
+    login();
+    const ui = UI.get();
+    ui.browser.browseTo(doc.path);
+    ui.browser.currentPage.waitForDisplayed();
+  });
+});
