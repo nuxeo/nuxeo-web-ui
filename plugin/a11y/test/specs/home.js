b1a66ee3308625027711a556fa007ba13bce7e52
Ricardo Dias <rdias@nuxeo.com>
2022-02-01 17:32:16 +0000

    WEBUI-666: increase number of a11y violations (possibly due to dependency update)

--- plugin/a11y/test/specs/home.js
+++ plugin/a11y/test/specs/home.js
@@ -6,20 +6,20 @@ import { reportA11y } from '../a11y-reporter.js';
 
 const EXPECTED_VIOLATIONS = {
   'aria-command-name': 1,
-  'aria-allowed-attr': 3,
+  'aria-allowed-attr': 4,
   'aria-required-parent': 9,
   'aria-tooltip-name': 1,
   'duplicate-id': 28,
   'landmark-one-main': 1,
   'meta-viewport': 1,
   'page-has-heading-one': 1,
-  region: 30,
+  region: 29,
   'nested-interactive': 13,
 };
 
 const EXPECTED_INCOMPLETE_VIOLATIONS = {
   'aria-allowed-role': 5,
-  'color-contrast': 0,
+  'color-contrast': 22,
 };
 
 describe('Nuxeo Home', () => {
