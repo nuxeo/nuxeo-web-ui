a1d1d734489bb88665d51deb1fc1fc8924d5a3bc
rakeshkumar1019 <srakeshkumar1019@gmail.com>
2022-07-04 12:36:22 +0530

    ex5: (funct) updated functional test

--- packages/nuxeo-web-ui-ftest/features/step_definitions/parent_inspector.js
+++ packages/nuxeo-web-ui-ftest/features/step_definitions/parent_inspector.js
@@ -1,34 +1,32 @@
 import { Then } from '@cucumber/cucumber';
-import pause from 'webdriverio/build/commands/browser/pause';
 
-Then('I can open current document information', function() {
+Then('I can see parent inspector icon and click on it', function() {
   this.ui.browser.parentInspectorButton.waitForVisible();
   this.ui.browser.parentInspectorButton.click();
   this.ui.browser.parentInspectorDialog.waitForVisible();
 });
 
-Then('I can see dialog showing the information for a given document', function() {
-  this.ui.browser.parentInspectorDataTitle === 'Title:';
-  this.ui.browser.parentInspectorDataPath === 'Path:';
-  this.ui.browser.parentInspectorDataUID === 'UID:';
-  this.ui.browser.parentInspectorDataFacets === 'Facets:';
-  this.ui.browser.parentInspectorDataSchema === 'Schemas:';
-});
-
-Then('I can see dialog showing some information for a given document', function() {
-  this.ui.browser.parentInspectorDataTitle === 'Title:';
-  this.ui.browser.parentInspectorDataPath === 'Path:';
-  this.ui.browser.parentInspectorDataFacets === 'Facets:';
-  this.ui.browser.waitForNotVisible(this.ui.browser.parentInspectorDataUID);
-  this.ui.browser.waitForNotVisible(this.ui.browser.parentInspectorDataSchema);
-});
-
 Then('I can close the parent inspector dialog box', function() {
-  this.ui.browser.parentInspectorClose.click();
-  pause(2000);
+  this.ui.browser.parentInspector.parentInspectorClose.click();
   this.ui.browser.waitForNotVisible(this.ui.browser.parentInspectorDialog);
 });
 
-Then('I cannot open current document information', function() {
+Then('I can see parent inspector dialog opened', function() {
+  this.ui.browser.parentInspector.title.waitForDisplayed();
+  this.ui.browser.parentInspector.path.waitForDisplayed();
+  this.ui.browser.parentInspector.uid.waitForDisplayed();
+  this.ui.browser.parentInspector.facets.waitForDisplayed();
+  this.ui.browser.parentInspector.schema.waitForDisplayed();
+});
+
+Then('I can see parent inspector dialog opened with partial parent inspector information', function() {
+  this.ui.browser.parentInspector.title.waitForDisplayed();
+  this.ui.browser.parentInspector.path.waitForDisplayed();
+  this.ui.browser.parentInspector.facets.waitForDisplayed();
+  this.ui.browser.waitForNotVisible(this.ui.browser.parentInspector.uid);
+  this.ui.browser.waitForNotVisible(this.ui.browser.parentInspector.schema);
+});
+
+Then('I cannot see parent inspector icon', function() {
   this.ui.browser.waitForNotVisible(this.ui.browser.parentInspectorButton);
 });
