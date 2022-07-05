11d6cfb66afb6ba3b11c0d7a333522ace30c555f
rakeshkumar1019 <srakeshkumar1019@gmail.com>
2022-07-05 13:05:24 +0530

    ex5: resolved comment

--- test/nuxeo-document-parent-inspector-button.test.js
+++ test/nuxeo-document-parent-inspector-button.test.js
@@ -25,6 +25,7 @@ const waitForDialogOpen = async (dialog) => {
     await flush();
   }
 };
+
 const waitForDialogClose = async (dialog) => {
   if (isElementVisible(dialog)) {
     await waitForEvent(dialog, 'iron-overlay-closed');
@@ -32,48 +33,42 @@ const waitForDialogClose = async (dialog) => {
   }
 };
 
+const document = {
+  'entity-type': 'document',
+  contextParameters: {
+    actionButton: {},
+    firstAccessibleAncestor: {
+      path: '/default-domain/Workspaces/File 1',
+      title: 'my file',
+      type: 'File',
+      uid: '7ertyyy-hdjkdks-874fghd',
+      facets: ['facet1', 'facet2'],
+      schemas: [
+        {
+          prefix: 'pre',
+          name: 'schema1',
+        },
+        {
+          prefix: 'pre',
+          name: 'schema2',
+        },
+      ],
+    },
+  },
+};
 suite('nuxeo-document-parent-inspector-button', () => {
   let button;
   let actionButton;
   const buildButton = async () => {
-    const documents = ['the content of this array is irrelevant'];
-
     actionButton = await fixture(
       html`
-        <nuxeo-document-parent-inspector-button .document="${documents}"> </nuxeo-document-parent-inspector-button>
+        <nuxeo-document-parent-inspector-button .document="${document}"> </nuxeo-document-parent-inspector-button>
       `,
     );
-
     await flush();
     return actionButton;
   };
   suite('Parent Inspector', () => {
-    const doc = {
-      'entity-type': 'document',
-      contextParameters: {
-        firstAccessibleAncestor: {
-          uid: '1',
-          title: 'File 1',
-          Path: '/default-domain/Workspaces/File 1',
-          facets: ['facet1', 'facet2'],
-          schemas: [
-            {
-              prefix: 'pre',
-              name: 'schema1',
-            },
-            {
-              prefix: 'pre',
-              name: 'schema2',
-            },
-          ],
-        },
-        type: 'File',
-      },
-      user: {
-        isAdministrator: true,
-      },
-    };
-
     test('Should open the parent inspector dialog', async () => {
       button = await buildButton();
       const actionBtn = button.$$('.action');
@@ -106,13 +101,15 @@ suite('nuxeo-document-parent-inspector-button', () => {
         'entity-type': 'document',
         contextParameters: {
           actionButton: {},
+          firstAccessibleAncestor: {
+            user: 'Administrator',
+            path: '/default-domain/Workspaces/File 1',
+            title: 'my file',
+            type: 'File',
+            uid: '7ertyyy-hdjkdks-874fghd',
+            isTrashed: true,
+          },
         },
-        user: 'Administrator',
-        path: '/default-domain/Workspaces/File 1',
-        title: 'my file',
-        type: 'File',
-        uid: '7ertyyy-hdjkdks-874fghd',
-        isTrashed: true,
       };
       actionButton.set('document', trashDocument);
       await flush();
@@ -120,138 +117,34 @@ suite('nuxeo-document-parent-inspector-button', () => {
       expect(isElementVisible(actionBtn)).to.be.false;
     });
 
-    test('Should not display parent inspector icon when user is not a Administrator/poweruser', async () => {
-      const trashDocument = {
-        'entity-type': 'document',
-        contextParameters: {
-          actionButton: {},
+    test('Should display title, UID, path, schemas and facets in parent inspector dialog', async () => {
+      button = await buildButton();
+      button.document = document;
+      button.currentUser = {
+        properties: {
+          username: 'John',
         },
-        user: 'Member',
-        path: '/default-domain/Workspaces/File 1',
-        title: 'my file',
-        type: 'File',
-        uid: '7ertyyy-hdjkdks-874fghd',
+        isAdministrator: true,
       };
-      doc.isAdministrator = false;
-      actionButton.set('document', trashDocument);
       await flush();
       const actionBtn = button.$$('.action');
-      expect(isElementVisible(actionBtn)).to.be.false;
+      const { dialog } = button.$;
+      actionBtn.click();
+      await waitForDialogOpen(dialog);
+      const title = button.$$('.table tr:nth-child(1) td:nth-child(2)');
+      const path = button.$$('.table tr:nth-child(2) td:nth-child(2)');
+      const uid = button.$$('.table tr:nth-child(3) td:nth-child(2)');
+      const facet1 = button.$$('.facetscontainer .facets .show-items:nth-child(1)');
+      const facet2 = button.$$('.facetscontainer .facets .show-items:nth-child(2)');
+      const schema1 = button.$$('.schemascontainer .schemas .show-items:nth-child(1)');
+      const schema2 = button.$$('.schemascontainer .schemas .show-items:nth-child(2)');
+      expect(title.innerHTML).to.equals('my file');
+      expect(path.innerHTML).to.equals('/default-domain/Workspaces/File 1');
+      expect(uid.innerHTML).to.equals('7ertyyy-hdjkdks-874fghd');
+      expect(facet1.innerHTML).to.equals('facet1');
+      expect(facet2.innerHTML).to.equals('facet2');
+      expect(schema1.innerHTML).to.equals('pre:schema1');
+      expect(schema2.innerHTML).to.equals('pre:schema2');
     });
-
-    test(
-      'Should display title, UID, path, schemas and facets in parent inspector dialog if user is administrator',
-      async () => {
-        const document = {
-          'entity-type': 'document',
-          contextParameters: {
-            actionButton: {},
-            firstAccessibleAncestor: {
-              path: '/default-domain/Workspaces/File 1',
-              title: 'my file',
-              type: 'File',
-              uid: '7ertyyy-hdjkdks-874fghd',
-              facets: ['facet1', 'facet2'],
-              schemas: [
-                {
-                  prefix: 'pre',
-                  name: 'schema1',
-                },
-                {
-                  prefix: 'pre',
-                  name: 'schema2',
-                },
-              ],
-            },
-          },
-          user: {
-            isAdministrator: true,
-          },
-        };
-        button = await buildButton();
-        button.document = document;
-        button.currentUser = {
-          properties: {
-            username: 'John',
-          },
-          isAdministrator: true,
-        };
-        await flush();
-        const actionBtn = button.$$('.action');
-        const { dialog } = button.$;
-        actionBtn.click();
-        await waitForDialogOpen(dialog);
-        const title = button.$$('.table tr:nth-child(1)');
-        const path = button.$$('.table tr:nth-child(2)');
-        const uid = button.$$('.table tr:nth-child(3)');
-        const facet1 = button.$$('.facetscontainer .facets .show-items:nth-child(1)');
-        const facet2 = button.$$('.facetscontainer .facets .show-items:nth-child(2)');
-        const schema1 = button.$$('.schemascontainer .schemas .show-items:nth-child(1)');
-        const schema2 = button.$$('.schemascontainer .schemas .show-items:nth-child(2)');
-        expect(title.innerHTML).to.contains('Title:');
-        expect(path.innerHTML).to.contains('Path:');
-        expect(uid.innerHTML).to.contains('UID:');
-        expect(facet1.innerHTML).to.equals('facet1');
-        expect(facet2.innerHTML).to.equals('facet2');
-        expect(schema1.innerHTML).to.equals('pre:schema1');
-        expect(schema2.innerHTML).to.equals('pre:schema2');
-      },
-    );
-
-    test(
-      'Should display title, path and facets and hide uid and schemas' +
-        ' in parent inspector dialog if user is not administrator',
-      async () => {
-        const document = {
-          'entity-type': 'document',
-          contextParameters: {
-            actionButton: {},
-            firstAccessibleAncestor: {
-              path: '/default-domain/Workspaces/File 1',
-              title: 'my file',
-              type: 'File',
-              uid: '7ertyyy-hdjkdks-874fghd',
-              facets: ['facet1', 'facet2'],
-              schemas: [
-                {
-                  prefix: 'pre',
-                  name: 'schema1',
-                },
-                {
-                  prefix: 'pre',
-                  name: 'schema2',
-                },
-              ],
-            },
-          },
-          user: {
-            isAdministrator: false,
-          },
-        };
-        button = await buildButton();
-        button.document = document;
-        button.currentUser = {
-          properties: {
-            username: 'Mary',
-          },
-          isAdministrator: false,
-        };
-        await flush();
-        const actionBtn = button.$$('.action');
-        const { dialog } = button.$;
-        actionBtn.click();
-        await waitForDialogOpen(dialog);
-        const title = button.$$('.table tr:nth-child(1)');
-        const path = button.$$('.table tr:nth-child(2)');
-        const uid = button.$$('.table tr:nth-child(3)');
-        const facets = button.$$('.facetscontainer .facets > .show-items');
-        const schemas = button.$$('.schemascontainer .schemas > .show-items');
-        expect(title.innerHTML).to.contains('Title:');
-        expect(path.innerHTML).to.contains('Path:');
-        expect(isElementVisible(uid)).to.be.false;
-        expect(isElementVisible(facets)).to.be.true;
-        expect(isElementVisible(schemas)).to.be.false;
-      },
-    );
   });
 });
