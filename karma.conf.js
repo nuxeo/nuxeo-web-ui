971d3ee01d589e2b69686073405aba6a8d5050ff
rakeshkumar1019 <srakeshkumar1019@gmail.com>
2022-10-13 13:42:54 +0530

    WEBUI-936: upgrade karma webdriverio lit-html geckodriverVersion dependencies

--- karma.conf.js
+++ karma.conf.js
@@ -28,6 +28,7 @@ if (process.env.SAUCE_USERNAME && process.env.SAUCE_ACCESS_KEY) {
       browserName: 'firefox',
       platform: 'Windows 10',
       version: 'latest',
+      geckodriverVersion: '0.30.0',
     },
     sl_latest_edge: {
       base: 'SauceLabs',
