7668e0ce36828eb01d20be87d6050b931d731553
Rajat Goel <rajat.goel@globallogic.com>
2022-11-19 18:45:46 +0530

    LTS2023: Added async await to axe-core

--- plugin/a11y/test/helpers/login.js
+++ plugin/a11y/test/helpers/login.js
@@ -1,13 +1,10 @@
 import Login from '@nuxeo/nuxeo-web-ui-ftest/pages/login';
-import UI from '@nuxeo/nuxeo-web-ui-ftest/pages/ui';
 
-const login = (username = 'Administrator', password = 'Administrator') => {
+const login = async (username = 'Administrator', password = 'Administrator') => {
   const logIn = Login.get();
-  logIn.username = username;
-  logIn.password = password;
-  logIn.submit();
-  const ui = UI.get();
-  ui.waitForVisible('nuxeo-page');
+  await logIn.setusername(username);
+  await logIn.setpassword(password);
+  await logIn.submit();
 };
 
 export default login;
