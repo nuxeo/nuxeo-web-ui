b735e7a4892d5658e4543b6728ffc60603e9f593
Pranit-Sotre <pranit.sadashivsotre@hyland.com>
2022-11-30 14:41:14 +0530

    WEBUI-986: add functional test to capture history data of document

--- packages/nuxeo-web-ui-ftest/pages/ui/history_table.js
+++ packages/nuxeo-web-ui-ftest/pages/ui/history_table.js
@@ -2,6 +2,30 @@ import BasePage from '../base';
 
 export default class HistoryTable extends BasePage {
   getHistory(event) {
-    return this.el.element(`///*[text()="${event}"]`);
+    return this.el.$(`///*[text()="${event}"]`);
+  }
+
+  get isHistoryTableVisible() {
+    return this.el.$('#table').waitForDisplayed();
+  }
+
+  get isHistoryTableFilled() {
+    this.el.waitForDisplayed('#table nuxeo-data-table-row');
+    return !this.el.$$('#table nuxeo-data-table-row').some((row) => row.getText().trim().length === 0);
+  }
+
+  waitForHasEntry(action, reverse) {
+    const { el } = this;
+    driver.waitUntil(
+      () => {
+        const cells = el.$$('#table nuxeo-data-table-cell');
+        if (reverse) {
+          return cells.every((cell) => cell.getText().trim() !== action);
+        }
+        return cells.some((cell) => cell.getText().trim() === action);
+      },
+      reverse ? 'The history does have such entry' : 'The history does not have such entry',
+    );
+    return true;
   }
 }
