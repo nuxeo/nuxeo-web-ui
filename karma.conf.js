0332413d5c92ee79ff2ee507d73917f3cb235591
rakeshkumar1019 <srakeshkumar1019@gmail.com>
2023-01-05 06:53:19 +0530

    update GeckoDriver Version 0.31.0

--- karma.conf.js
+++ karma.conf.js
@@ -28,7 +28,7 @@ if (process.env.SAUCE_USERNAME && process.env.SAUCE_ACCESS_KEY) {
       browserName: 'firefox',
       platform: 'Windows 10',
       version: 'latest',
-      geckodriverVersion: '0.30.0',
+      geckodriverVersion: '0.31.0',
     },
     sl_latest_edge: {
       base: 'SauceLabs',
