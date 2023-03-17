ee9fe8bb7886dd5db2cd191bec9f7efe884014da
rakeshkumar1019 <srakeshkumar1019@gmail.com>
2023-03-17 17:33:53 +0530

    run a11y in debug mode

--- elements/nuxeo-document-creation/nuxeo-document-creation-behavior.js
+++ elements/nuxeo-document-creation/nuxeo-document-creation-behavior.js
@@ -18,7 +18,6 @@ limitations under the License.
 import { config } from '@nuxeo/nuxeo-elements';
 import { FormatBehavior } from '@nuxeo/nuxeo-ui-elements/nuxeo-format-behavior.js';
 import { RoutingBehavior } from '@nuxeo/nuxeo-ui-elements/nuxeo-routing-behavior.js';
-import { join } from '@nuxeo/nuxeo-elements/utils';
 
 let schemaFetcher = null;
 
@@ -87,7 +86,7 @@ export const DocumentCreationBehavior = [
         schemaFetcher = document.createElement('nuxeo-resource');
         this.shadowRoot.appendChild(schemaFetcher);
       }
-      schemaFetcher.path = `${join('path/', this.targetPath)}/@emptyWithDefault`;
+      schemaFetcher.path = `path/${this.targetPath}/@emptyWithDefault`;
       schemaFetcher.params = { type: this.selectedDocType.type };
       schemaFetcher.headers = {
         properties: '*',
