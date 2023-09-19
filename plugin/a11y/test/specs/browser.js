b8975b348228c6cbb5e23d5a3087bb02c2fb0efb
poonam yadav <poonam.yadav@contractors.onbase.com>
2023-09-19 18:30:59 +0530

    test commit

--- plugin/a11y/test/specs/browser.js
+++ plugin/a11y/test/specs/browser.js
@@ -1,7 +1,6 @@
 import '../imports';
-import UI from '@nuxeo/nuxeo-web-ui-ftest/pages/ui';
 import documentService from '@nuxeo/nuxeo-web-ui-ftest/features/step_definitions/support/services/documentService';
-import login from '../helpers/login';
+import { authRedirect } from '../helpers/login';
 import { reportA11y } from '../a11y-reporter.js';
 
 const EXPECTED_VIOLATIONS = {
@@ -36,10 +35,11 @@ describe('Nuxeo Browser', () => {
 
   after(async () => documentService.reset());
 
-  reportA11y(EXPECTED_VIOLATIONS, EXPECTED_INCOMPLETE_VIOLATIONS, () => {
-    login();
-    const ui = UI.get();
-    ui.browser.browseTo(doc.path);
-    ui.browser.currentPage.waitForDisplayed();
+  reportA11y(EXPECTED_VIOLATIONS, EXPECTED_INCOMPLETE_VIOLATIONS, async () => {
+    await authRedirect(browser, `#!/browse${doc.path}`);
+    await browser.$('#documentViewsItems nuxeo-page-item.iron-selected').waitForExist({ timeout: 15000 });
+    const elementName = await browser.getAttribute('#documentViewsItems nuxeo-page-item.iron-selected', 'name');
+    const context = await browser.$(`#nxContent [name='${elementName}']`);
+    return context;
   });
 });
