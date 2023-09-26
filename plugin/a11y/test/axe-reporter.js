482a472d6d0ddd3461aa1f36e799bbed5f18bdbb
swarnadipa-dev <swarnadipa.choudhury@globallogic.com>
2023-09-26 17:07:08 +0530

    WEBUI-1352: update lts2021 from node js branch

--- plugin/a11y/test/axe-reporter.js
+++ plugin/a11y/test/axe-reporter.js
@@ -1,35 +1,40 @@
 // eslint-disable-next-line import/no-unresolved
 import { source } from 'axe-core';
 
-export function runAxeCore() {
-  // inject the axe-core lib
-  browser.execute(source);
+class AxeCore {
+  async run() {
+    await browser.execute(source);
+    const options = {
+      runOnly: {
+        type: 'tag',
+        values: ['ACT', 'best-practice', 'wcag2a', 'wcag2aa'],
+      },
+    };
+    // run inside browser and get results
+    const results = await browser.executeAsync((opts, done) => {
+      // eslint-disable-next-line no-undef
+      axe
+        .run(opts)
+        .then((res) => done(res))
+        .catch((err) => {
+          throw err;
+        });
+    }, options);
+    return this.process(await results);
+  }
 
-  // https://github.com/dequelabs/axe-core/blob/develop/doc/API.md
-  const options = {
-    runOnly: {
-      type: 'tag',
-      values: ['ACT', 'best-practice', 'wcag2a', 'wcag2aa'],
-    },
-  };
-  // run inside browser and get results
-  const results = browser.executeAsync((opts, done) => {
-    // eslint-disable-next-line no-undef
-    axe
-      .run(opts)
-      .then((res) => done(res))
-      .catch((err) => {
-        throw err;
-      });
-  }, options);
-
-  return {
-    results,
-    incomplete: results.incomplete.map((a) => {
-      return { id: a.id, issues: a.nodes.length };
-    }),
-    violations: results.violations.map((a) => {
-      return { id: a.id, issues: a.nodes.length };
-    }),
-  };
+  process(results) {
+    return {
+      results,
+      incomplete: results.incomplete.map((a) => {
+        return { id: a.id, issues: a.nodes.length };
+      }),
+      violations: results.violations.map((a) => {
+        return { id: a.id, issues: a.nodes.length };
+      }),
+    };
+  }
+}
+export async function runAxeCore() {
+  return new AxeCore().run();
 }
