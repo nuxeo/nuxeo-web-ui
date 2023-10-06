59b96ff1076baa522a66f152127f0adee7cd9d31
alokhyland <Alok.Ranjan@Hyland.com>
2023-10-06 14:00:27 +0530

    WEBUI-1317: Accessibility checks are failing after upgrade NodeJS 18

--- plugin/a11y/test/helpers/login.js
+++ plugin/a11y/test/helpers/login.js
@@ -1,13 +1,13 @@
 import Login from '@nuxeo/nuxeo-web-ui-ftest/pages/login';
 import UI from '@nuxeo/nuxeo-web-ui-ftest/pages/ui';
 
-const login = (username = 'Administrator', password = 'Administrator') => {
+const login = async (username = 'Administrator', password = 'Administrator') => {
   const logIn = Login.get();
-  logIn.username = username;
-  logIn.password = password;
-  logIn.submit();
+  await logIn.username(username);
+  await logIn.password(password);
+  await logIn.submit();
   const ui = UI.get();
-  ui.waitForVisible('nuxeo-page');
+  await ui.waitForVisible('nuxeo-page');
 };
 
 export default login;
