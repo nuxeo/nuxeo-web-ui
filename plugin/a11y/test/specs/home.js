6401a129b1155479c24da71a8905b5b7a9933a4d
alokhyland <Alok.Ranjan@Hyland.com>
2023-10-10 20:36:02 +0530

    changed incomplete violation

--- plugin/a11y/test/specs/home.js
+++ plugin/a11y/test/specs/home.js
@@ -5,22 +5,12 @@ import login from '../helpers/login';
 import { reportA11y } from '../a11y-reporter.js';
 
 const EXPECTED_VIOLATIONS = {
-  'aria-allowed-attr': 1,
-  'aria-command-name': 1,
-  'aria-required-children': 1,
-  'aria-tooltip-name': 1,
-  'color-contrast': 12,
-  'landmark-one-main': 1,
+  'html-has-lang': 1,
   'meta-viewport': 1,
-  'nested-interactive': 13,
-  'page-has-heading-one': 1,
-  region: 79,
+  region: 1,
 };
 
-const EXPECTED_INCOMPLETE_VIOLATIONS = {
-  'aria-allowed-role': 5,
-  'color-contrast-enhanced': 10,
-};
+const EXPECTED_INCOMPLETE_VIOLATIONS = {};
 
 describe('Nuxeo Home', () => {
   before(async () => {
