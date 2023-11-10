54d64477026708edd86ddc17bf63c7a4266f5466
rakeshkumar1019 <srakeshkumar1019@gmail.com>
2023-11-10 10:30:32 +0530

    loglevel info

--- packages/nuxeo-web-ui-ftest/wdio.conf.js
+++ packages/nuxeo-web-ui-ftest/wdio.conf.js
@@ -140,7 +140,7 @@ exports.config = {
   sync: true,
   //
   // Level of logging verbosity: trace | debug | info | warn | error | silent
-  logLevel: 'trace',
+  logLevel: 'info',
   //
   // Enables colors for log output.
   coloredLogs: true,
