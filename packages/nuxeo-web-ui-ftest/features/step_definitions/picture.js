7848b1c5f3c45ae65293b245a873b48e40893f93
Nishant0928 <Nishant.Singh@contractors.onbase.com>
2023-12-29 11:31:53 +0530

    WEBUI-1412: update functional test for picture 3.0.x

--- packages/nuxeo-web-ui-ftest/features/step_definitions/picture.js
+++ packages/nuxeo-web-ui-ftest/features/step_definitions/picture.js
@@ -1,7 +1,12 @@
 import { Then } from '../../node_modules/@cucumber/cucumber';
 
-Then('I can see the picture formats panel', function() {
-  const page = this.ui.browser.documentPage(this.doc.type);
-  page.waitForVisible();
-  page.el.$('.additional').waitForVisible('nuxeo-picture-formats').should.be.true;
+Then('I can see the picture formats panel', async function() {
+  const uiBrowser = await this.ui.browser;
+  const page = await uiBrowser.documentPage(this.doc.type);
+  await page.waitForVisible();
+  const additionalPage = await page.el.$('.additional');
+  await additionalPage.waitForVisible();
+  const pageElement = await additionalPage.$('nuxeo-picture-formats');
+  const element = await pageElement.waitForVisible();
+  await element.should.be.true;
 });
