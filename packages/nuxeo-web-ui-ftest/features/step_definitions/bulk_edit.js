2e1517c944d7ac8af32b5b9778e597a2f6b3f011
alokhyland <Alok.Ranjan@Hyland.com>
2024-01-03 22:34:38 +0530

    WEBUI-1374: Update ftest for features: Bulk_edit

--- packages/nuxeo-web-ui-ftest/features/step_definitions/bulk_edit.js
+++ packages/nuxeo-web-ui-ftest/features/step_definitions/bulk_edit.js
@@ -1,20 +1,26 @@
 import { Then } from '../../node_modules/@cucumber/cucumber';
 
-Then('I click the bulk edit button with {string} layout', function(layoutName) {
-  this.ui.browser.selectionToolbar.clickResultsActionMenu(`nuxeo-edit-documents-button[layout=${layoutName}]`);
+Then('I click the bulk edit button with {string} layout', async function(layoutName) {
+  const browser = await this.ui.browser;
+  await browser.waitForVisible();
+  const selectionToolbarElem = await browser.selectionToolbar;
+  await selectionToolbarElem.waitForVisible();
+  await selectionToolbarElem.clickResultsActionMenu(`nuxeo-edit-documents-button[layout=${layoutName}]`);
 });
 
-Then('I can bulk edit multiple properties in {string} layout:', function(layoutName, table) {
-  const action = this.ui.bulkEdit(`nuxeo-edit-documents-button[layout="${layoutName}"]`);
-  action.dialog.waitForDisplayed();
-  action.editMultipleOptions(table);
-  action.saveButton.click();
+Then('I can bulk edit multiple properties in {string} layout:', async function(layoutName, table) {
+  const action = await this.ui.bulkEdit(`nuxeo-edit-documents-button[layout="${layoutName}"]`);
+  const dialogElem = await action.dialog;
+  await dialogElem.waitForDisplayed();
+  await action.editMultipleOptions(table);
+  const saveButtonElem = await action.saveButton;
+  await saveButtonElem.click();
 });
 
 Then('I see a toast notification with the following message {string}', async function(message) {
   const notificationMessage = await this.ui.getToastMessage(message);
-  const trimmedMessage = await message.trim().replace(/"/g, '');
-  await notificationMessage.should.be.equals(trimmedMessage);
+  const trimmedMessage = message.trim().replace(/"/g, '');
+  notificationMessage.should.be.equals(trimmedMessage);
 });
 
 Then('I click the toast notification dismiss button', async function() {
