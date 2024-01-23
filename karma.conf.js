c9ada89a6c0691b75256dc85098a74c102a7a8ed
rahuljain-dev <rahul.jain@contractors.onbase.com>
2024-01-23 17:57:39 +0530

    fixing firefox version to 98 and geckodriver 30

--- karma.conf.js
+++ karma.conf.js
@@ -34,7 +34,7 @@ if (process.env.SAUCE_USERNAME && process.env.SAUCE_ACCESS_KEY) {
       browserName: 'firefox',
       platform: 'Windows 10',
       version: '98.0',
-      geckodriverVersion: '0.34.0',
+      geckodriverVersion: '0.30.0',
     },
     sl_latest_safari: {
       base: 'SauceLabs',
