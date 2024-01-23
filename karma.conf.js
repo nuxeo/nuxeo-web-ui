c8046ccfa3cecfa4e6f5d8cc7b9a8b6746da05dd
rahuljain-dev <rahul.jain@contractors.onbase.com>
2024-01-23 17:46:18 +0530

    latest geckodriver version

--- karma.conf.js
+++ karma.conf.js
@@ -33,8 +33,8 @@ if (process.env.SAUCE_USERNAME && process.env.SAUCE_ACCESS_KEY) {
       base: 'SauceLabs',
       browserName: 'firefox',
       platform: 'Windows 10',
-      version: 'latest',
-      geckodriverVersion: '0.30.0',
+      version: '98.0',
+      geckodriverVersion: '0.34.0',
     },
     sl_latest_safari: {
       base: 'SauceLabs',
