3002e2ef6bea9e7faf19be1719c2f5a08a63e34a
rahuljain-dev <rahul.jain@contractors.onbase.com>
2024-01-25 12:39:24 +0530

    added comment

--- karma.conf.js
+++ karma.conf.js
@@ -33,7 +33,7 @@ if (process.env.SAUCE_USERNAME && process.env.SAUCE_ACCESS_KEY) {
       base: 'SauceLabs',
       browserName: 'firefox',
       platform: 'Windows 10',
-      version: 'latest',
+      version: 'latest', // latest
       geckodriverVersion: '0.30.0',
     },
     sl_latest_safari: {
