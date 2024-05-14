c87a5e4b18f901d1e9114bdc7e0f0772c0cd6f51
alokhyland <Alok.Ranjan@Hyland.com>
2024-05-14 13:59:05 +0530

    WEBUI-1511: Own Code Static Scan : Open Redirect

--- elements/routing.js
+++ elements/routing.js
@@ -196,7 +196,7 @@ app.router = {
     }
     const isFullpath = /^http(s)?:\/\//.test(path);
     if (isFullpath) {
-      window.location = path;
+      window.location = encodeURI(path);
     } else {
       page(path);
     }
