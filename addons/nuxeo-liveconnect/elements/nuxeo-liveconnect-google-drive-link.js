e16c224f4f0fe06fac41a9d9a8777a184c6deb52
alokhyland <139326173+alokhyland@users.noreply.github.com>
2024-09-16 16:18:14 +0530

    Revert "WEBUI-1580: CSP code fix for nuxeo live connect addon"
    This reverts commit 7670ffd919792c1c05fb559b64f7d13b3ea71cd6.

--- addons/nuxeo-liveconnect/elements/nuxeo-liveconnect-google-drive-link.js
+++ addons/nuxeo-liveconnect/elements/nuxeo-liveconnect-google-drive-link.js
@@ -64,7 +64,7 @@ Polymer({
       is-available="{{isProviderAvailable}}"
     ></nuxeo-liveconnect-google-drive-provider>
     <template is="dom-if" if="[[isProviderAvailable]]">
-      <a href="#" on-tap="_openPicker">
+      <a href="javascript:undefined" on-tap="_openPicker">
         <iron-icon src="[[importPath]]images/google_drive.png"></iron-icon>
         [[i18n('liveconnectImportActions.googledrive', 'Google Drive')]]
       </a>
@@ -78,8 +78,7 @@ Polymer({
     this.$.provider.updateProviderInfo();
   },
 
-  _openPicker(e) {
-    e.preventDefault();
+  _openPicker() {
     this.$.provider.openPicker();
   },
 });
