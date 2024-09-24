91c3abaad1646c706df2623907be54a1227d127b
rahuljain-dev <rahul.jain@contractors.onbase.com>
2024-09-24 15:09:07 +0530

    WEBUI-1583: ftest pipeline failing in nuxeo-web-ui

--- packages/nuxeo-web-ui-ftest/scripts/test.js
+++ packages/nuxeo-web-ui-ftest/scripts/test.js
@@ -112,7 +112,7 @@ if (process.env.DRIVER_VERSION == null) {
   console.log(`${version} detected.`);
   const match = version && version.match(/([0-9]+)\./);
   if (match) {
-    const checkVersion = 128; // fixed chrome version to avoid functional test breaking
+    const checkVersion = match[1];
     try {
       done = fetch(`https://googlechromelabs.github.io/chrome-for-testing/LATEST_RELEASE_${checkVersion}`).then(
         (response) => {
