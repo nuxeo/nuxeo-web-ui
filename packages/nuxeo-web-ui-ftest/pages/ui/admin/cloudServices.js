119f2570de2ab68c30bb27807510681c2bfb6e60
alokhyland <Alok.Ranjan@Hyland.com>
2024-10-23 18:28:54 +0530

    remove condition

--- packages/nuxeo-web-ui-ftest/pages/ui/admin/cloudServices.js
+++ packages/nuxeo-web-ui-ftest/pages/ui/admin/cloudServices.js
@@ -163,7 +163,7 @@ export default class CloudServices extends BasePage {
       .$$('nuxeo-data-table[name="table"] nuxeo-data-table-row:not([header])')
       .map((img) => img.$('nuxeo-data-table-cell span[name="id"]').getText());
     const index = deleted.findIndex((currenTitle) => currenTitle === clientId);
-    if (index !== -1 && rows && rows.length > 0) {
+    if (index !== -1) {
       const rowEle = await rows[index].$('[name="delete"]');
       await rowEle.click();
       await driver.alertAccept();
