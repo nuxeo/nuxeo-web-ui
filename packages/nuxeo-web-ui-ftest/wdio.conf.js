35562317932769ee19f522c42c2ae5cc05bf27bd
rahuljain-dev <rahul.jain@contractors.onbase.com>
2024-12-04 14:46:26 +0530

    wdio basic changes

--- packages/nuxeo-web-ui-ftest/wdio.conf.js
+++ packages/nuxeo-web-ui-ftest/wdio.conf.js
@@ -27,7 +27,7 @@ const capability = {
   maxInstances: 1,
   browserName: process.env.BROWSER,
   acceptInsecureCerts: true,
-  browserVersion: '130.0.6723.116',
+  browserVersion: 'stable',
 };
 
 const options = {};
@@ -35,7 +35,6 @@ const options = {};
 switch (capability.browserName) {
   case 'chrome':
     options.args = ['--no-sandbox'];
-    options.w3c = false;
 
     if (process.env.HEADLESS) {
       options.args.push('--window-size=1920,1080');
