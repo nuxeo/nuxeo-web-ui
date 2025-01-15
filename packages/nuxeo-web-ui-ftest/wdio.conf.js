4df8d1b110806c75d83f6b835382301e8f1b750e
rahuljain-dev <rahul.jain@contractors.onbase.com>
2025-01-15 15:37:52 +0530

    test wdio config

--- packages/nuxeo-web-ui-ftest/wdio.conf.js
+++ packages/nuxeo-web-ui-ftest/wdio.conf.js
@@ -35,7 +35,7 @@ const options = {};
 switch (capability.browserName) {
   case 'chrome':
     options.args = ['--no-sandbox'];
-    options.w3c = false;
+    options.w3c = [false];
 
     if (process.env.HEADLESS) {
       options.args.push('--window-size=1920,1080');
