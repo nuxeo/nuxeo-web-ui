df3a2f0b5e5e396b1233d039ce941d0fb94f5649
rakeshkumar1019 <srakeshkumar1019@gmail.com>
2025-01-22 20:40:29 +0530

    WEBUI-1616: revert nuxeo document tree

--- elements/nuxeo-document-tree/nuxeo-document-tree.js
+++ elements/nuxeo-document-tree/nuxeo-document-tree.js
@@ -63,7 +63,6 @@ Polymer({
       :host([dir='rtl']) {
         --nuxeo-tree-children-theme: {
           padding-right: 1em;
-          padding-left: 0;
         }
       }
 
@@ -71,10 +70,6 @@ Polymer({
         padding: 5px 0;
         overflow: auto;
         height: calc(100vh - 72px - (var(--nuxeo-app-top, 0) + var(--nuxeo-app-bottom, 0)));
-      }
-
-      :host([dir='rtl']) .content {
-        padding-right: 24px;
       }
 
       .node-name {
@@ -105,6 +100,10 @@ Polymer({
         width: 1.3rem;
         margin-right: -1.6em;
         margin-top: -0.07rem;
+      }
+
+      :host([dir='rtl']) iron-icon {
+        margin-right: 0;
       }
 
       [toggle] {
