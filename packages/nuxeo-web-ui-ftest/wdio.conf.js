b4e63ce2c7c268ff823a59d24a06c45caff2ab6f
rahuljain-dev <rahul.jain@contractors.onbase.com>
2025-01-27 16:03:39 +0530

    test cft 131

--- packages/nuxeo-web-ui-ftest/wdio.conf.js
+++ packages/nuxeo-web-ui-ftest/wdio.conf.js
@@ -27,7 +27,7 @@ const capability = {
   maxInstances: 1,
   browserName: process.env.BROWSER,
   acceptInsecureCerts: true,
-  browserVersion: '130.0.6723.116',
+  browserVersion: '131.0.6778.70',
 };
 
 const options = {};
