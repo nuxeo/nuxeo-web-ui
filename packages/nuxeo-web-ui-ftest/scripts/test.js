1332fbcce510cdba1c02b68410a11d4be246c9b4
rahuljain-dev <rahul.jain@contractors.onbase.com>
2025-01-31 12:06:28 +0530

    WEBUI-1628: corrected variables naming

--- packages/nuxeo-web-ui-ftest/scripts/test.js
+++ packages/nuxeo-web-ui-ftest/scripts/test.js
@@ -141,9 +141,9 @@ if (process.env.DRIVER_VERSION == null) {
         if (response.ok) {
           return response
             .text()
-            .then((response1) => {
-              const obj = JSON.parse(response1);
-              const cftVersion = obj.channels.Stable.version;
+            .then((responseJSON) => {
+              const responseObj = JSON.parse(responseJSON);
+              const cftVersion = responseObj.channels.Stable.version;
               // eslint-disable-next-line no-console
               console.log(`ChromeForTesting ${cftVersion} detected.`);
             })
