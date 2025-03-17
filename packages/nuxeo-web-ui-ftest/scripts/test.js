c73e02e17e7e83262a1f968ed9ff6cb09e080f57
Madhur Kulshrestha <madhur.kulshrestha@hyland.com>
2025-03-17 12:27:56 +0530

    rectified changes

--- packages/nuxeo-web-ui-ftest/scripts/test.js
+++ packages/nuxeo-web-ui-ftest/scripts/test.js
@@ -135,28 +135,6 @@ if (process.env.DRIVER_VERSION == null) {
       console.error('unable to fetch ChromeDriver version: ', e);
     }
   }
-  try {
-    done = fetch(`https://googlechromelabs.github.io/chrome-for-testing/last-known-good-versions.json`).then(
-      (response) => {
-        if (response.ok) {
-          return response
-            .text()
-            .then((responseJSON) => {
-              const responseObj = JSON.parse(responseJSON);
-              const cftVersion = responseObj.channels.Stable.version;
-              // eslint-disable-next-line no-console
-              console.log(`ChromeForTesting ${cftVersion} detected.`);
-            })
-            .catch((e) => {
-              console.error('unable to parse Chrome for testing browser version: ', e);
-            });
-        }
-        console.error('unable to fetch Chrome for testing browser version: ', response);
-      },
-    );
-  } catch (e) {
-    console.error('unable to fetch Chrome for testing browser version ', e);
-  }
 }
 try {
   done = fetch(`https://googlechromelabs.github.io/chrome-for-testing/last-known-good-versions.json`).then(
