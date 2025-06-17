2cdf636634647490ea9c4a263e458bb12ee1cceb
rakeshkumar1019 <srakeshkumar1019@gmail.com>
2025-06-17 09:58:27 +0530

    WEBUI-1678: inflight vulnerability overrides glob

--- scripts/merge-messages.js
+++ scripts/merge-messages.js
@@ -8,24 +8,23 @@ const CWD = process.cwd();
 mkdirSync(DEST, { recursive: true });
 
 const BUNDLES = (process.env.NUXEO_PACKAGES || '')
-  .split(/[\s,]+/)
+  .split(/\s|,+/)
   .filter(Boolean)
   .filter((p) => existsSync(`addons/${p}`));
 
 const SOURCES = [...BUNDLES.map((b) => `addons/${b}/i18n`), 'node_modules/@nuxeo/nuxeo-ui-elements/i18n'];
 
-glob('i18n/messages*.json', (_, files) =>
-  files.forEach((file) => {
-    const messages = require(`${CWD}/${file}`);
-    const filename = path.basename(file);
+const files = glob.sync('i18n/messages*.json');
+files.forEach((file) => {
+  const messages = require(`${CWD}/${file}`);
+  const filename = path.basename(file);
 
-    SOURCES.forEach((s) => {
-      const p = `${CWD}/${s}/${filename}`;
-      if (existsSync(p)) {
-        Object.assign(messages, require(p));
-      }
-    });
+  SOURCES.forEach((s) => {
+    const p = `${CWD}/${s}/${filename}`;
+    if (existsSync(p)) {
+      Object.assign(messages, require(p));
+    }
+  });
 
-    writeFileSync(`${DEST}/${filename}`, JSON.stringify(messages, null, 2));
-  }),
-);
+  writeFileSync(`${DEST}/${filename}`, JSON.stringify(messages, null, 2));
+});
