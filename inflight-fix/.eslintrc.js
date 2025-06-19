5be4122b7ba6ae51326668218a9f5fffd47487df
rakeshkumar1019 <srakeshkumar1019@gmail.com>
2025-06-19 07:27:37 +0530

    custome package

---
+++ inflight-fix/.eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  rules: {
+    'import/no-extraneous-dependencies': 'off',
+    'no-use-before-define': 'off',
+    'no-shadow': 'off',
+    'prefer-spread': 'off',
+    'prefer-rest-params': 'off',
+  },
+};
