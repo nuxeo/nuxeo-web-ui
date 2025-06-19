25beb0f75d9f9d1fb757efc317fa74b5436fb41a
rakeshkumar1019 <srakeshkumar1019@gmail.com>
2025-06-19 09:33:15 +0530

    back to 0

--- inflight-fix/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  rules: {
-    'import/no-extraneous-dependencies': 'off',
-    'no-use-before-define': 'off',
-    'no-shadow': 'off',
-    'prefer-spread': 'off',
-    'prefer-rest-params': 'off',
-  },
-};
