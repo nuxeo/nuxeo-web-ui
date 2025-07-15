63c53587a6fa9488ae964267f9e87098a530dee1
Nishant0928 <Nishant.Singh@contractors.onbase.com>
2025-07-15 17:18:55 +0530

    pr review changes

--- packages/nuxeo-web-ui-ftest/features/step_definitions/browser.js
+++ packages/nuxeo-web-ui-ftest/features/step_definitions/browser.js
@@ -153,9 +153,10 @@ Then('I can move selection up', async function() {
 Then('I can see the {string} child document is at position {int}', async function(title, pos) {
   const browser = await this.ui.browser;
   await browser.waitForVisible();
-  const childIndex = await browser.indexOfChild(title);
-  if (childIndex !== pos - 1) {
-    throw new Error(`${title} child document not present at expected position`);
+  const index = await browser.indexOfChild(title);
+  if (index !== pos - 1) {
+    throw new Error(`${title} child document not present at expected position ${pos}, 
+      but found at position ${index + 1}`);
   }
 });
 
