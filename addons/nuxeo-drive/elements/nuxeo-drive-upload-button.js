e8946fba301fda7c9e0bf8f730b4f604a47656ea
Swarnadipa Choudhury <swarnadipa.choudhury@globallogic.com>
2025-07-29 16:18:47 +0530

    converted to polymer3

--- addons/nuxeo-drive/elements/nuxeo-drive-upload-button.js
+++ addons/nuxeo-drive/elements/nuxeo-drive-upload-button.js
@@ -14,28 +14,33 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
-import '@polymer/paper-button/paper-button.js';
-import '@polymer/paper-icon-button/paper-icon-button.js';
-import '@polymer/paper-toast/paper-toast.js';
-import '@polymer/iron-flex-layout/iron-flex-layout-classes.js';
-import '@polymer/polymer/lib/elements/dom-if.js';
 
-import '@nuxeo/nuxeo-ui-elements/nuxeo-resource.js';
-import '@nuxeo/nuxeo-ui-elements/nuxeo-dialog.js';
-import '@nuxeo/nuxeo-ui-elements/nuxeo-tooltip.js';
-import '@nuxeo/nuxeo-drive/nuxeo-drive-desktop-packages.js';
-
+import { html, PolymerElement } from '@polymer/polymer/polymer-element.js';
+import { mixinBehaviors } from '@polymer/polymer/lib/legacy/class.js';
 import { I18nBehavior } from '@nuxeo/nuxeo-ui-elements/nuxeo-i18n-behavior.js';
 import { FiltersBehavior } from '@nuxeo/nuxeo-ui-elements/nuxeo-filters-behavior.js';
-
 import './nuxeo-drive-icons.js';
 
-const baseUrl = window.nuxeo?.baseUrl || `${window.location.origin}${window.location.pathname}`;
+window.nuxeo = window.nuxeo || {};
+const baseUrl = window.nuxeo.baseUrl || window.location.origin + window.location.pathname;
 
-class NuxeoDriveUploadButton extends PolymerElement {
+class NuxeoDriveUploadButton extends mixinBehaviors([I18nBehavior, FiltersBehavior], PolymerElement) {
   static get is() {
     return 'nuxeo-drive-upload-button';
+  }
+
+  static get properties() {
+    return {
+      document: Object,
+      /**
+       * `true` if the action should display the label, `false` otherwise.
+       */
+      showLabel: {
+        type: Boolean,
+        reflectToAttribute: true,
+        value: false,
+      },
+    };
   }
 
   static get template() {
@@ -44,7 +49,7 @@ class NuxeoDriveUploadButton extends PolymerElement {
 
       <nuxeo-resource id="token" path="/token" params='{"application": "Nuxeo Drive"}'></nuxeo-resource>
 
-      <template is="dom-if" if="[[_isAvailable(document, blob)]]">
+      <template is="dom-if" if="[[_isAvailable(document)]]">
         <div class="action" on-tap="_go">
           <paper-icon-button
             noink
@@ -71,26 +76,17 @@ class NuxeoDriveUploadButton extends PolymerElement {
     `;
   }
 
-  static get properties() {
-    return {
-      document: Object,
-      /**
-       * `true` if the action should display the label, `false` otherwise.
-       */
-      showLabel: {
-        type: Boolean,
-        reflectToAttribute: true,
-        value: false,
-      },
-    };
-  }
-
-  static get behaviors() {
-    return [I18nBehavior, FiltersBehavior];
-  }
-
   _isAvailable(doc) {
-    return this.hasPermission(doc, 'Write') && this.hasFacet(doc, 'Folderish') && !this.isProxy(doc);
+    if (!doc) return false;
+
+    return (
+      this.hasPermission &&
+      this.hasFacet &&
+      this.isProxy &&
+      this.hasPermission(doc, 'Write') &&
+      this.hasFacet(doc, 'Folderish') &&
+      !this.isProxy(doc)
+    );
   }
 
   _go() {
@@ -111,8 +107,10 @@ class NuxeoDriveUploadButton extends PolymerElement {
   }
 
   get directTransferUrl() {
-    const path = this.document?.path?.slice(1) || '';
-    return ['nxdrive://direct-transfer', baseUrl.replace('://', '/'), path].join('/');
+    const finalUrl = ['nxdrive://direct-transfer', baseUrl.replace('://', '/'), this.document?.path?.slice(1)].join(
+      '/',
+    );
+    return finalUrl;
   }
 }
 
