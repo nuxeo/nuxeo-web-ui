53db21283af1dca0a9225a5671b1e1dd644d6e15
vaibhavagarwal4-lab <Vaibhav.Agarwal@hyland.com>
2025-09-07 23:27:04 +0530

    WEBUI 1754: disabled up and down arrow keys lts 2025

--- elements/nuxeo-admin/nuxeo-analytics.js
+++ elements/nuxeo-admin/nuxeo-analytics.js
@@ -74,8 +74,13 @@ Polymer({
   ready() {
     if (super.ready) super.ready();
     const listbox = this.$$('paper-listbox');
+
     listbox.addEventListener('keydown', (e) => {
       e.preventDefault();
+      if (e.key === 'ArrowUp' || e.key === 'ArrowDown') {
+        e.preventDefault();
+        return;
+      }
       if (e.key === 'ArrowLeft') {
         listbox.selectPrevious();
         listbox.selectedItem.focus();
