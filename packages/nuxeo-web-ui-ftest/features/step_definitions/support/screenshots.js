f8f171aa351f6b385890596a7363fa036cb76624
rakeshkumar1019 <srakeshkumar1019@gmail.com>
2025-09-10 11:06:09 +0530

    ELEMENTS-1831: fix ftest expired & revert mkdri

--- packages/nuxeo-web-ui-ftest/features/step_definitions/support/screenshots.js
+++ packages/nuxeo-web-ui-ftest/features/step_definitions/support/screenshots.js
@@ -1,12 +1,12 @@
 import * as path from 'path';
-import { mkdirp } from 'mkdirp';
+import * as mkdirp from 'mkdirp';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { After, Status } from '@cucumber/cucumber';
 
 After(async function(scenario) {
   const { status } = scenario.result;
   if (process.env.SCREENSHOTS_PATH && status === Status.FAILED) {
-    await mkdirp(process.env.SCREENSHOTS_PATH);
+    mkdirp.sync(process.env.SCREENSHOTS_PATH);
     const filename = path.join(process.env.SCREENSHOTS_PATH, `${scenario.pickle.name} (${status}).png`);
     const screenshot = await browser.saveScreenshot(filename);
     this.attach(screenshot, 'image/png');
