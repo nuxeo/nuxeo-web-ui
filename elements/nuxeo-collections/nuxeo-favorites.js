139873727ec0b11e6e2b598a068f9b85db623bb3
Swarnadipa Choudhury <swarnadipa.choudhury@globallogic.com>
2025-09-16 17:35:48 +0530

    logo getting focused. search queue view tabbable

--- elements/nuxeo-collections/nuxeo-favorites.js
+++ elements/nuxeo-collections/nuxeo-favorites.js
@@ -144,7 +144,11 @@ Polymer({
         empty-label-when-filtered="[[i18n('favorites.empty')]]"
       >
         <template>
-          <div tabindex="0" class$="[[_computedClass(selected)]]" on-keydown="_handleKeyNav">
+          <div
+            tabindex$="{{_computeTabAndLastIndex(index)}}"
+            class$="[[_computedClass(selected)]]"
+            on-keydown="_handleKeyNav"
+          >
             <div class="list-item-box">
               <div class="list-item-info">
                 <div class="list-item-thumbnail">
@@ -250,5 +254,11 @@ Polymer({
 
   _handleKeyNav(e) {
     handleVerticalKeyNavigation(e, '.list-item');
+  },
+
+  _computeTabAndLastIndex(index) {
+    const tabindex = index + 1;
+    this._lastIndex = this.nxProvider.resultsCount;
+    return tabindex.toString();
   },
 });
