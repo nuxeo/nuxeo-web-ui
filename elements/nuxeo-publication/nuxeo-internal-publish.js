5a8d019b1edb9bcaf6e04885a218b3503cc5462a
vaibhavagarwal4-lab <Vaibhav.Agarwal@hyland.com>
2025-10-16 23:45:30 +0530

    ftest fix

--- elements/nuxeo-publication/nuxeo-internal-publish.js
+++ elements/nuxeo-publication/nuxeo-internal-publish.js
@@ -235,7 +235,7 @@ Polymer({
     };
     if (this.selectedRendition === 'default') {
       parameters.defaultRendition = true;
-    } else if (this.selectedRendition !== 'none') {
+    } else {
       parameters.renditionName = this.selectedRendition;
     }
     return parameters;
