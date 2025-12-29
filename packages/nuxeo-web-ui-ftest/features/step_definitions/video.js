a6bac298f1e47165f437eb849db177039c7cc0a8
Madhur Kulshrestha <madhur.kulshrestha@hyland.com>
2025-12-29 21:08:01 +0530

    Disabled lint on console

--- packages/nuxeo-web-ui-ftest/features/step_definitions/video.js
+++ packages/nuxeo-web-ui-ftest/features/step_definitions/video.js
@@ -15,27 +15,29 @@ Then('I can see the video storyboard', async function() {
     async () => {
       const uiBrowser = await this.ui.browser;
       const page = await uiBrowser.documentPage(this.doc.type);
+      /* eslint-disable no-console */
       if (!(await page.isVisible())) {
-        console.log("Page is not visible yet");
+        console.log('Page is not visible yet');
         return false;
       }
       const videoViewer = await page.el.$('nuxeo-video-viewer');
       if (!(await videoViewer.isDisplayed())) {
-        console.log("Viewer is not displayed yet");
+        console.log('Viewer is not displayed yet');
         return false;
       }
       const storyBoard = await videoViewer.$('#storyboard');
       if (!(await storyBoard.isExisting())) {
-        console.log("Storyboard does not exist yet");
+        console.log('Storyboard does not exist yet');
         return false;
       }
       if (!(await storyBoard.isVisible())) {
-        console.log("Storyboard is not visible yet.. Refreshing");
+        console.log('Storyboard is not visible yet.. Refreshing');
         await driver.execute(async () => Nuxeo.UI.app.refresh());
         await driver.pause(1000);
         return false;
       }
-      console.log("FOUND !!!!");
+      console.log('FOUND !!!!');
+      /* eslint-enable no-console */
       return true;
     },
     {
