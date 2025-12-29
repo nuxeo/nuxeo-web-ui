296bca054b2cebc3e60b2c072eb3f06ed36ec6b2
Madhur Kulshrestha <madhur.kulshrestha@hyland.com>
2025-12-29 21:02:02 +0530

    Changes in video.feature

--- packages/nuxeo-web-ui-ftest/features/step_definitions/video.js
+++ packages/nuxeo-web-ui-ftest/features/step_definitions/video.js
@@ -16,21 +16,29 @@ Then('I can see the video storyboard', async function() {
       const uiBrowser = await this.ui.browser;
       const page = await uiBrowser.documentPage(this.doc.type);
       if (!(await page.isVisible())) {
+        console.log("Page is not visible yet");
         return false;
       }
       const videoViewer = await page.el.$('nuxeo-video-viewer');
       if (!(await videoViewer.isDisplayed())) {
+        console.log("Viewer is not displayed yet");
         return false;
       }
       const storyBoard = await videoViewer.$('#storyboard');
       if (!(await storyBoard.isExisting())) {
+        console.log("Storyboard does not exist yet");
         return false;
       }
-      const { height } = await storyBoard.getSize();
-      return height > 0;
+      if (!(await storyBoard.isVisible())) {
+        console.log("Storyboard is not visible yet.. Refreshing");
+        await driver.execute(async () => Nuxeo.UI.app.refresh());
+        await driver.pause(1000);
+        return false;
+      }
+      console.log("FOUND !!!!");
+      return true;
     },
     {
-      interval: 500,
       timeoutMsg: 'I cannot see the video storyboard',
     },
   );
