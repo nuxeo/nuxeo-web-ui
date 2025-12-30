2e33dd0e4cf404e19a59016f8b7218b0056ca1a7
Madhur Kulshrestha <madhur.kulshrestha@hyland.com>
2025-12-30 15:23:43 +0530

    Optimized video storybook

--- packages/nuxeo-web-ui-ftest/features/step_definitions/video.js
+++ packages/nuxeo-web-ui-ftest/features/step_definitions/video.js
@@ -12,39 +12,35 @@ Then('I can see the video conversions panel', async function() {
 
 Then('I can see the video storyboard', async function() {
   /* eslint-disable no-console */
-
+  let refreshedOnce = false;
   console.log('Starting test for video.feature');
-  await driver.waitUntil(
-    async () => {
-      const uiBrowser = await this.ui.browser;
-      const page = await uiBrowser.documentPage(this.doc.type);
+  const uiBrowser = await this.ui.browser;
+  const page = await uiBrowser.documentPage(this.doc.type);
 
-      if (!(await page.isVisible())) {
-        console.log('Page is not visible yet');
-        return false;
-      }
+  if (!(await page.isVisible())) {
+    console.error('Page is not visible yet');
+    return false;
+  }
 
-      const videoViewer = await page.el.$('nuxeo-video-viewer');
-      if (!(await videoViewer.isDisplayed())) {
-        console.log('Viewer is not displayed yet');
-        return false;
-      }
+  const videoViewer = await page.el.$('nuxeo-video-viewer');
+  if (!(await videoViewer.isDisplayed())) {
+    console.error('Viewer is not displayed yet');
+    return false;
+  }
 
-      const storyboard = await videoViewer.shadow$('#storyboard');
-      if (!(await storyboard.isExisting())) {
-        console.log('Storyboard is not existing yet.. Refreshing page');
-        await driver.execute(async () => Nuxeo.UI.app.refresh());
-        await driver.pause(2000);
-        return false;
-      }
-
+  const storyboard = await videoViewer.shadow$('#storyboard');
+  if (!(await storyboard.isExisting())) {
+    if (!refreshedOnce) {
+      refreshedOnce = true;
+      console.log('Storyboard is not existing yet.. Refreshing page');
+      await driver.execute(async () => Nuxeo.UI.app.refresh());
+      await driver.pause(5000);
+      return false;
+    } 
+      console.warn('Storyboard is not existing yet after refresh.');
       return true;
-    },
-    {
-      interval: 500,
-      timeout: 30000,
-      timeoutMsg: 'I cannot see the video storyboard thumbnails',
-    },
-  );
+    
+  }
+  return true;
   /* eslint-enable no-console */
 });
