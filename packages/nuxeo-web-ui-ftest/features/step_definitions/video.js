5734e16b89c43d2aad7dac141163dfdfa638366c
Madhur Kulshrestha <madhur.kulshrestha@hyland.com>
2025-12-30 15:59:37 +0530

    Optimized video storybook

--- packages/nuxeo-web-ui-ftest/features/step_definitions/video.js
+++ packages/nuxeo-web-ui-ftest/features/step_definitions/video.js
@@ -14,32 +14,41 @@ Then('I can see the video storyboard', async function() {
   /* eslint-disable no-console */
   let refreshedOnce = false;
   console.log('Starting test for video.feature');
-  const uiBrowser = await this.ui.browser;
-  const page = await uiBrowser.documentPage(this.doc.type);
+  await driver.waitUntil(
+    async () => {
+      const uiBrowser = await this.ui.browser;
+      const page = await uiBrowser.documentPage(this.doc.type);
 
-  if (!(await page.isVisible())) {
-    console.error('Page is not visible yet');
-    return false;
-  }
+      if (!(await page.isVisible())) {
+        console.error('Page is not visible yet');
+        return false;
+      }
 
-  const videoViewer = await page.el.$('nuxeo-video-viewer');
-  if (!(await videoViewer.isDisplayed())) {
-    console.error('Viewer is not displayed yet');
-    return false;
-  }
+      const videoViewer = await page.el.$('nuxeo-video-viewer');
+      if (!(await videoViewer.isDisplayed())) {
+        console.error('Viewer is not displayed yet');
+        return false;
+      }
 
-  const storyboard = await videoViewer.shadow$('#storyboard');
-  if (!(await storyboard.isExisting())) {
-    if (!refreshedOnce) {
-      refreshedOnce = true;
-      console.log('Storyboard is not existing yet.. Refreshing page');
-      await driver.execute(async () => Nuxeo.UI.app.refresh());
-      await driver.pause(5000);
-      return false;
-    }
-    console.warn('Storyboard is not existing yet after refresh.');
-    return true;
-  }
-  return true;
+      const storyboard = await videoViewer.shadow$('#storyboard');
+      if (!(await storyboard.isExisting())) {
+        if (!refreshedOnce) {
+          refreshedOnce = true;
+          console.log('Storyboard is not existing yet.. Refreshing page');
+          await driver.execute(async () => Nuxeo.UI.app.refresh());
+          await driver.pause(5000);
+          return false;
+        } 
+          console.warn('Storyboard is not existing yet after refresh.');
+          return true;
+        
+      }
+      return true;
+    },
+    {
+      interval: 500,
+      timeoutMsg: 'I cannot see the video storyboard',
+    },
+  );
   /* eslint-enable no-console */
 });
