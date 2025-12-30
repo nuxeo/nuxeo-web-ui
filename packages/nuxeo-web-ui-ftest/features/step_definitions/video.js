cd7b6cb8ec5f066fe8aa641047bf09abbc7532ea
Madhur Kulshrestha <madhur.kulshrestha@hyland.com>
2025-12-30 14:06:33 +0530

    Optimized video storybook

--- packages/nuxeo-web-ui-ftest/features/step_definitions/video.js
+++ packages/nuxeo-web-ui-ftest/features/step_definitions/video.js
@@ -1,18 +1,6 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { Then } from '@cucumber/cucumber';
 
-async function hasRenderedThumbnails(videoViewer) {
-  const thumbnails = await videoViewer.shadow$('#thumbnails');
-  if (!(await thumbnails.isExisting())) {
-    return false;
-  }
-
-  return driver.execute(
-    (el) => Array.from(el.children).some((c) => c.offsetHeight > 0 && c.offsetWidth > 0),
-    thumbnails,
-  );
-}
-
 Then('I can see the video conversions panel', async function() {
   const uiBrowser = await this.ui.browser;
   const page = await uiBrowser.documentPage(this.doc.type);
@@ -24,13 +12,8 @@ Then('I can see the video conversions panel', async function() {
 
 Then('I can see the video storyboard', async function() {
   /* eslint-disable no-console */
-  const WAIT_INTERVAL = 500;
-  const FIRST_WAIT_MS = 5000; // before refresh
-  const SECOND_WAIT_MS = 5000; // after refresh
 
   console.log('Starting test for video.feature');
-  let refreshed = false;
-  const start = Date.now();
   await driver.waitUntil(
     async () => {
       const uiBrowser = await this.ui.browser;
@@ -40,40 +23,30 @@ Then('I can see the video storyboard', async function() {
         console.log('Page is not visible yet');
         return false;
       }
+
       const videoViewer = await page.el.$('nuxeo-video-viewer');
       if (!(await videoViewer.isDisplayed())) {
         console.log('Viewer is not displayed yet');
         return false;
       }
 
-      // Phase 1: normal wait
-      if (await hasRenderedThumbnails(videoViewer)) {
-        console.log('Found thumbnails !!');
-        return true;
-      }
-      // Phase 2: refresh once after timeout
-      if (!refreshed && Date.now() - start > FIRST_WAIT_MS) {
-        console.log('Storyboard not found, refreshing page once...');
-
-        await driver.execute(() => Nuxeo.UI.app.refresh());
-        refreshed = true;
-
-        // Give UI time to re-bootstrap
-        await driver.pause(1000);
-      }
-
-      // Phase 3: wait again after refresh
-      if (refreshed && Date.now() - start > FIRST_WAIT_MS + SECOND_WAIT_MS) {
-        console.log('WAITING AFTER REFRESH');
+      const thumbnails = await videoViewer.shadow$('#thumbnails');
+      if (!(await thumbnails.isExisting())) {
+        console.log('Thumbnails not existing yet. Refreshing after 10 sec');
+        await driver.pause(10000);
+        console.log('Refreshing...');
+        await driver.execute(async () => Nuxeo.UI.app.refresh());
+        console.log('Waiting for page to load...');
+        await driver.pause(2000);
         return false;
       }
 
-      console.log('Not found !');
-      return false;
+      console.log('Found thumbnails!!');
+      return true;
     },
     {
-      interval: WAIT_INTERVAL,
-      timeout: FIRST_WAIT_MS + SECOND_WAIT_MS + 2000,
+      interval: 500,
+      timeout: 30000,
       timeoutMsg: 'I cannot see the video storyboard thumbnails',
     },
   );
