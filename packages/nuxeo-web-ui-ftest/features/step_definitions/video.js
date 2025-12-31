85949206f9dea5f03a200c084f52f31fc3855a81
Madhur Kulshrestha <157124712+madhurkulshrestha-hyland@users.noreply.github.com>
2025-12-31 12:31:04 +0530

    WEBUI-1903 : Optimized video.feature and csv-import.feature [LTS-2023] (#2845)
    * Optimized video.feature
    
    * Changes to browser.js
    
    * Changes to browser.js
    
    * Removed console log
    
    * Removed isVisible
    
    * Changes in video.feature
    
    * Disabled lint on console
    
    * Optimized video storybook
    
    * Optimized video storybook
    
    * Optimized video storybook
    
    * Optimized video storybook
    
    * Optimized video storybook
    
    * Optimized video storybook
    
    * Optimized video storybook
    
    * Optimized video storybook
    
    * Optimized video storybook
    
    * Optimized video storybook
    
    * Optimized video storybook
    
    * Optimized video storybook
    
    * Optimized video storybook
    
    * Re-enabled all ftests

--- packages/nuxeo-web-ui-ftest/features/step_definitions/video.js
+++ packages/nuxeo-web-ui-ftest/features/step_definitions/video.js
@@ -4,34 +4,43 @@ import { Then } from '@cucumber/cucumber';
 Then('I can see the video conversions panel', async function() {
   const uiBrowser = await this.ui.browser;
   const page = await uiBrowser.documentPage(this.doc.type);
-  page.waitForVisible();
+  await page.waitForVisible();
   const element = await page.el.$('nuxeo-video-conversions');
   const elementVisible = await element.waitForVisible();
   await elementVisible.should.be.true;
 });
 
 Then('I can see the video storyboard', async function() {
-  driver.waitUntil(
+  let refreshedOnce = false;
+  await driver.waitUntil(
     async () => {
       const uiBrowser = await this.ui.browser;
       const page = await uiBrowser.documentPage(this.doc.type);
-      if (!page.isVisible()) {
+
+      if (!(await page.isVisible())) {
         return false;
       }
-      const videoViewer = await page.el.element('nuxeo-video-viewer');
-      if (!videoViewer.isVisible()) {
+
+      const videoViewer = await page.el.$('nuxeo-video-viewer');
+      if (!(await videoViewer.isDisplayed())) {
         return false;
       }
-      const storyBoard = await videoViewer.element('#storyboard');
-      const boardVisible = await storyBoard.isVisible();
-      if (boardVisible !== true) {
-        await driver.execute(async () => Nuxeo.UI.app.refresh());
-        await driver.pause(1000);
-        return false;
+
+      const storyboard = await videoViewer.shadow$('#storyboard');
+      if (!(await storyboard.isExisting())) {
+        if (!refreshedOnce) {
+          refreshedOnce = true;
+          await driver.execute(async () => Nuxeo.UI.app.refresh());
+          await driver.pause(5000);
+          return false;
+        }
+        console.warn('Storyboard is not existing yet even after refreshing page.');
+        return true;
       }
       return true;
     },
     {
+      interval: 500,
       timeoutMsg: 'I cannot see the video storyboard',
     },
   );
