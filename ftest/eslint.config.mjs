8e163e9c0101fcaaa8e197abe26513a6d68e710a
Madhur Kulshrestha <madhur.kulshrestha@hyland.com>
2026-03-19 15:55:56 +0530

    Removed dependency of eslint-config and upgraded eslint libraries

---
+++ ftest/eslint.config.mjs
@@ -0,0 +1,40 @@
+import globals from 'globals';
+
+export default [
+  {
+    files: ['ftest/**/*.js'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+
+        // legacy ftest globals
+        $: 'writable',
+        addedComments: 'writable',
+        driver: 'readonly',
+        fixtures: 'readonly',
+        liveCollections: 'readonly',
+        liveDocuments: 'readonly',
+        runningWorkflows: 'readonly',
+        users: 'readonly',
+        groups: 'readonly',
+        browser: 'readonly',
+      },
+    },
+    rules: {
+      'new-cap': [
+        'error',
+        {
+          capIsNewExceptions: ['Given', 'When', 'Then', 'After', 'Before'],
+        },
+      ],
+      'no-unused-expressions': 'off',
+      'no-else-return': 'off',
+      'no-return-assign': 'off',
+      'no-throw-literal': 'off',
+      'no-underscore-dangle': 'off',
+      'prefer-destructuring': 'off',
+      radix: 'off',
+    },
+  },
+];
