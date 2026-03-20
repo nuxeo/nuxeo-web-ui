8e3b402fae8dbd49c1d3b2bad1b564be18c37d4b
Nishant0928 <Nishant.Singh@contractors.onbase.com>
2026-03-19 23:11:00 -0700

    WEBUI-149: do not populate the metadata of all the imported documents with the metadata of the first doc

--- elements/document/nuxeo-document-import.js
+++ elements/document/nuxeo-document-import.js
@@ -32,6 +32,7 @@ import '@nuxeo/nuxeo-elements/nuxeo-connection.js';
 import '@nuxeo/nuxeo-elements/nuxeo-document.js';
 import '@nuxeo/nuxeo-elements/nuxeo-resource.js';
 import '@nuxeo/nuxeo-elements/nuxeo-operation.js';
+import { config } from '@nuxeo/nuxeo-elements';
 import { NotifyBehavior } from '@nuxeo/nuxeo-elements/nuxeo-notify-behavior.js';
 import '@nuxeo/nuxeo-ui-elements/widgets/nuxeo-select.js';
 import { UploaderBehavior } from '@nuxeo/nuxeo-ui-elements/widgets/nuxeo-uploader-behavior.js';
@@ -820,6 +821,11 @@ Polymer({
     },
 
     _importWithPropertiesError: String,
+
+    _loadingEmptyFile: {
+      type: Boolean,
+      value: false,
+    },
   },
 
   listeners: {
@@ -1035,16 +1041,32 @@ Polymer({
 
   _loadFile(docData, title) {
     let properties = {};
-    if (docData && Object.keys(docData).length > 0) {
+    const hasDocData = docData && Object.keys(docData).length > 0;
+    if (hasDocData) {
       this.targetPath = docData.parent;
       this.selectedDocType = this._importDocTypes.find((type) => type.id === docData.type.id);
       ({ properties } = JSON.parse(JSON.stringify(docData.document)));
+    } else {
+      this._loadingEmptyFile = true;
     }
     if (title) {
       properties['dc:title'] = title;
     }
     this._docProperties = properties;
-    return this._updateDocument();
+    const updatePromise = this._updateDocument();
+    if (!hasDocData && updatePromise) {
+      return updatePromise.then(
+        (result) => result,
+        (err) => {
+          this._loadingEmptyFile = false;
+          throw err;
+        },
+      );
+    }
+    if (!hasDocData) {
+      this._loadingEmptyFile = false;
+    }
+    return updatePromise;
   },
 
   _nextFile() {
@@ -1073,18 +1095,19 @@ Polymer({
     if (!this._isValidFileIndex(index)) {
       throw new Error(`invalid file index: ${index}`);
     } else if (this.docIdx !== index && (this.docIdx < 0 || this._validate())) {
-      if (this.docIdx > -1) {
-        this._storeFile(this.docIdx);
+      const previousIndex = this.docIdx;
+      const targetFile = this._getFile(index);
+      const willLoadEmpty = !(targetFile && targetFile.checked && targetFile.docData);
+      if (willLoadEmpty) {
+        this._loadingEmptyFile = true;
       }
-      const previousFile = this._getCurrentFile();
+      if (previousIndex > -1) {
+        this._storeFile(previousIndex);
+      }
       this.docIdx = index;
       const currentFile = this._getCurrentFile();
-      if (currentFile.checked) {
-        // load the file's own data
+      if (currentFile.checked && currentFile.docData) {
         await this._loadFile(currentFile.docData);
-      } else if (previousFile) {
-        // load the previous file's data
-        await this._loadFile(previousFile.docData, currentFile.name);
       } else {
         await this._loadFile({}, currentFile.name);
       }
@@ -1161,6 +1184,7 @@ Polymer({
     this._docProperties = {};
     this._creating = false;
     this._initializingDoc = false;
+    this._loadingEmptyFile = false;
     this._importWithPropertiesError = '';
     this._importErrorMessage = '';
     this.$.uploadFiles.value = '';
@@ -1409,6 +1433,38 @@ Polymer({
     return this._docProperties;
   },
 
+  /**
+   * Override behavior's newDocument so that when loading a file with no saved data,
+   * we return a fresh document with only dc:title set. This prevents Nature, Subjects,
+   * Coverage, etc. from being copied from the first document or server defaults.
+   * When _loadingEmptyFile we do not mutate the fetched doc (it may be cached).
+   */
+  newDocument(type, properties) {
+    const { resource } = this;
+    resource.path = `path/${this.targetPath}/@emptyWithDefault`;
+    resource.params = { type: this.selectedDocType.type };
+    resource.headers = {
+      properties: '*',
+      'fetch-document': 'properties',
+      'translate-directoryEntry': 'label',
+    };
+    resource.enrichers = config.get('enrichers', {});
+    return resource.get().then((doc) => {
+      if (this._loadingEmptyFile) {
+        const title = (properties && properties['dc:title']) || (doc.properties && doc.properties['dc:title']) || '';
+        const freshDoc = JSON.parse(JSON.stringify(doc));
+        freshDoc.properties = { 'dc:title': title };
+        return freshDoc;
+      }
+      if (properties && Object.keys(properties).length > 0) {
+        const freshDoc = JSON.parse(JSON.stringify(doc));
+        freshDoc.properties = JSON.parse(JSON.stringify(properties));
+        return freshDoc;
+      }
+      return doc;
+    });
+  },
+
   _computedCheckItem(e) {
     if (e.base && e.base.checked) {
       return 'icons:check-circle';
@@ -1506,8 +1562,13 @@ Polymer({
 
   /**
    * Retrieves and creates the layout for the current document type.
+   * When called with 2 args it is from the behavior's observer (selectedDocType, parent).
+   * Skip observer updates while loading empty file so Next/Previous don't get overwritten by cached doc.
    */
   _updateDocument() {
+    if (arguments.length === 2 && this._loadingEmptyFile) {
+      return;
+    }
     this._initializingDoc = true;
     if (!this._isValidType(this.selectedDocType) || !this.parent) {
       this.document = null;
@@ -1519,6 +1580,20 @@ Polymer({
 
     return this.newDocument(this.selectedDocType.type, this._getDocumentProperties()).then((document) => {
       document.parentRef = this.parent.uid;
+      const forceRemount = this._loadingEmptyFile || this.customizing;
+      if (forceRemount) {
+        this.document = null;
+        return new Promise((resolve) => {
+          this.async(() => {
+            this.document = document;
+            this._initializingDoc = false;
+            if (this._loadingEmptyFile) {
+              this._loadingEmptyFile = false;
+            }
+            resolve();
+          });
+        });
+      }
       // disable controls while the layout loads
       if (
         !this.document ||
