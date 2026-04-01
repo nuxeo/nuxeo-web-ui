3eedb93610dae709d6a9c33fb663e7008eea45b4
Naveen Konda <naveen.konda@hyland.com>
2026-04-01 16:50:54 +0530

    ELEMENTS-1834: Fix actorExists method to correctly select user elements and remove unnecessary trim

--- packages/nuxeo-web-ui-ftest/pages/ui/browser/document_task.js
+++ packages/nuxeo-web-ui-ftest/pages/ui/browser/document_task.js
@@ -39,10 +39,10 @@ export default class DocumentTask extends BasePage {
   }
 
   async actorExists(element, actor) {
-    const users = await element.elements('nuxeo-user-tag .tag a');
+    const users = await element.elements('nuxeo-user-tag .tag .username-container');
     for (let i = 0; i < users.length; i++) {
       // eslint-disable-next-line no-await-in-loop
-      const user = (await users[i].getText()).trim();
+      const user = await users[i].getText();
       if (user === actor) {
         return true;
       }
