6f239ca860c5a9d49b83d3a7701296759099687e
Madhur Kulshrestha <madhur.kulshrestha@hyland.com>
2026-04-13 11:33:38 +0530

    Enabled all Ftests

--- packages/nuxeo-web-ui-ftest/wdio.conf.js
+++ packages/nuxeo-web-ui-ftest/wdio.conf.js
@@ -124,7 +124,7 @@ export const config = {
   // WebdriverIO allows it to run your tests in arbitrary locations (e.g. locally or
   // on a remote machine).
   runner: 'local',
-  specs: [path.join(process.cwd(), './features/tasks.feature')],
+  specs: [path.join(process.cwd(), './features/*.feature')],
   // check http://webdriver.io/guide/testrunner/debugging.html for more info on debugging with wdio
   debug: process.env.DEBUG,
   execArgv: process.env.DEBUG ? ['--inspect'] : [],
