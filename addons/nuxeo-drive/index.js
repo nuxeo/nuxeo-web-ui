1a263ec51b7d4e81cffd3750431f12a9cce24853
Swarnadipa Choudhury <swarnadipa.choudhury@globallogic.com>
2026-04-20 12:01:05 +0530

    LTS-2025: feat(WEBUI-1963): add Direct Download to Nuxeo Drive action

--- addons/nuxeo-drive/index.js
+++ addons/nuxeo-drive/index.js
@@ -19,6 +19,7 @@ import './elements/nuxeo-drive-sync-toggle-button.js';
 import './elements/nuxeo-drive-edit-button.js';
 import './elements/nuxeo-drive-page.js';
 import './elements/nuxeo-drive-upload-button.js';
+import './elements/nuxeo-drive-download-button.js';
 
 import html from './nuxeo-drive.html';
 
