7a5cb5f4adbc7dfb0fccd3ceeb4589c90f41b37e
Swarnadipa Choudhury <swarnadipa.choudhury@globallogic.com>
2026-06-01 10:27:37 +0530

    refactor: extract compress URL logic to shared utilities
    - Add buildDirectTransferUrl and compressDirectTransferUrl to nuxeo-drive-utils.js
    - Refactor nuxeo-drive-upload-button to use shared compression functions
    - Update upload button tests for new utility functions
    - Improve addGoSuite test isolation with conditional _navigate stub
    - Fix test file imports and remove unused dependencies

--- addons/nuxeo-drive/elements/nuxeo-drive-upload-button.js
+++ addons/nuxeo-drive/elements/nuxeo-drive-upload-button.js
@@ -20,7 +20,7 @@ import { mixinBehaviors } from '@polymer/polymer/lib/legacy/class.js';
 import { I18nBehavior } from '@nuxeo/nuxeo-ui-elements/nuxeo-i18n-behavior.js';
 import { FiltersBehavior } from '@nuxeo/nuxeo-ui-elements/nuxeo-filters-behavior.js';
 import './nuxeo-drive-icons.js';
-import { base64UrlSafeEncode, fetchTokenAndToggleDialog } from './nuxeo-drive-utils.js';
+import { base64UrlSafeEncode, navigateAndShowFallback } from './nuxeo-drive-utils.js';
 
 window.nuxeo = window.nuxeo || {};
 const baseUrl = window.nuxeo.baseUrl || window.location.origin + window.location.pathname;
@@ -46,24 +46,12 @@ class NuxeoDriveUploadButton extends mixinBehaviors([I18nBehavior, FiltersBehavi
         type: Boolean,
         value: false,
       },
-
-      _opened: {
-        type: Boolean,
-        value: false,
-      },
-
-      _hasToken: {
-        type: Boolean,
-        value: false,
-      },
     };
   }
 
   static get template() {
     return html`
       <style include="nuxeo-action-button-styles"></style>
-
-      <nuxeo-resource id="token" path="/token" params='{"application": "Nuxeo Drive"}'></nuxeo-resource>
 
       <template is="dom-if" if="[[_isAvailable(document)]]">
         <div class="action" on-click="_go">
@@ -78,7 +66,7 @@ class NuxeoDriveUploadButton extends mixinBehaviors([I18nBehavior, FiltersBehavi
         </div>
       </template>
 
-      <nuxeo-dialog id="dialog" with-backdrop no-cancel-on-outside-click>
+      <nuxeo-dialog id="dialog" with-backdrop>
         <style>
           #dialog {
             margin-top: 0;
@@ -101,18 +89,6 @@ class NuxeoDriveUploadButton extends mixinBehaviors([I18nBehavior, FiltersBehavi
             margin: 0 0 16px;
           }
 
-          .launch-btn {
-            background-color: var(--nuxeo-primary-color, #0066ff);
-            color: #fff;
-            text-transform: uppercase;
-            font-size: 0.9em;
-            padding: 0.5em 1em;
-          }
-
-          .launch-btn[disabled] {
-            background-color: var(--disabled-text-color, #9e9e9e);
-          }
-
           .close-btn {
             border: 1px solid var(--nuxeo-primary-color, #0066ff);
             color: var(--nuxeo-primary-color, #0066ff);
@@ -121,49 +97,29 @@ class NuxeoDriveUploadButton extends mixinBehaviors([I18nBehavior, FiltersBehavi
             padding: 0.5em 1em;
           }
 
+          .buttons {
+            justify-content: flex-end;
+          }
+
           .install-link {
             display: inline-block;
             margin-top: 4px;
             font-size: 0.9em;
           }
-
-          .dialog-content .install-hint {
-            border-left: 4px solid var(--nuxeo-primary-color, #0066ff);
-            color: var(--primary-text-color);
-            padding-left: 8px;
-            margin: 12px 0 8px;
-            font-size: 0.85em;
-          }
-
-          .dialog-content .install-prompt {
-            color: var(--primary-text-color, #333);
-            margin: 15px 0 8px;
-            font-size: 0.9em;
-          }
-
-          .buttons {
-            justify-content: space-between;
-          }
         </style>
         <div class="dialog-content">
           <h1>[[i18n('driveButton.dialog.heading')]]</h1>
           <p>[[i18n('driveButton.dialog.description')]]</p>
-          <template is="dom-if" if="[[_opened]]">
-            <p class="install-hint">[[i18n('driveButton.dialog.install.hint')]]</p>
-          </template>
           <template is="dom-if" if="[[_installExpanded]]">
-            <p class="install-prompt">[[i18n('driveButton.dialog.install.prompt')]]</p>
+            <p>[[i18n('driveButton.dialog.install.prompt')]]</p>
             <nuxeo-drive-desktop-packages></nuxeo-drive-desktop-packages>
           </template>
           <template is="dom-if" if="[[!_installExpanded]]">
-            <a class="install-link" href="#" on-click="_toggleInstall">[[i18n('driveButton.dialog.install.link')]]</a>
+            <a class="install-link" href="#" on-click="_toggleInstall">[[i18n('driveButton.install.dialog.hint')]]</a>
           </template>
         </div>
         <div class="buttons">
           <paper-button dialog-dismiss class="close-btn">[[i18n('command.close')]]</paper-button>
-          <paper-button class="launch-btn" on-click="_openDrive" noink disabled$="[[_opened]]"
-            >[[i18n('driveButton.dialog.open')]]</paper-button
-          >
         </div>
       </nuxeo-dialog>
 
@@ -183,21 +139,12 @@ class NuxeoDriveUploadButton extends mixinBehaviors([I18nBehavior, FiltersBehavi
   }
 
   _go() {
-    if (this.$.dialog.opened) {
-      return;
+    try {
+      this._installExpanded = false;
+      navigateAndShowFallback(this, this.directTransferUrl);
+    } catch (e) {
+      this._showError(e.message);
     }
-
-    fetchTokenAndToggleDialog(this);
-  }
-
-  _openDrive() {
-    this._opened = true;
-    this._installExpanded = true;
-    this._navigate(this.directTransferUrl);
-  }
-
-  _navigate(url) {
-    globalThis.location.href = url;
   }
 
   _toggleInstall(e) {
