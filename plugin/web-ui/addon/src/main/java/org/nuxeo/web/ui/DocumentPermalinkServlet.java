cfa791dffed6fee0cab8c8b8a4e863af7df7e99e
Madhur Kulshrestha <madhur.kulshrestha@hyland.com>
2026-08-27 14:25:32 +0530

    WEBUI-1728: use jakarta.servlet namespace (Nuxeo 2025 / Tomcat 10)

--- plugin/web-ui/addon/src/main/java/org/nuxeo/web/ui/DocumentPermalinkServlet.java
+++ plugin/web-ui/addon/src/main/java/org/nuxeo/web/ui/DocumentPermalinkServlet.java
@@ -18,9 +18,9 @@ package org.nuxeo.web.ui;
 
 import java.io.IOException;
 
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 /**
  * Redirects RFC 3986 compliant document permalinks ({@code /ui/doc?id=<docId>}) to the Web UI
